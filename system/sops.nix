c9f22ac21a1898c45c03d580345c988dda89a875
raab <rob.w.lago@gmail.com>
2023-01-28 08:56:17 -0500

    fixing repo name

--- system/sops.nix
+++ system/sops.nix
@@ -3,7 +3,7 @@
   sops.defaultSopsFile = ./secrets/secrets.yaml;
   sops.age.keyFile = "/var/lib/sops-nix/key.txt";
   sops.age.generateKey = true;
-  sops.age.sshKeyPaths = [ "/nix/persist/system/etc/ssh/ssh_host_ed25519_key" ]; #"/nix/persist/home/raab/.config/sops/age/keys.txt"
+  sops.age.sshKeyPaths = [ "/nix/persist/system/etc/ssh/ssh_host_ed25519_key" ];
   sops.gnupg.sshKeyPaths = [];
   sops.secrets = {
     git_config = {
