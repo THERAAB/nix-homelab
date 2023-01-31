dc8b987e25dd4e9fbea9774e41519c8fe2b9f26a
raab <rob.w.lago@gmail.com>
2023-01-31 16:36:15 -0500

    updating tailscale ip

--- system/home-assistant/shopping-list.nix
+++ system/home-assistant/shopping-list.nix
@@ -1,17 +1,25 @@
 { config, pkgs, ... }:
 let
   devices = (import ./devices.properties.nix);
-  shopping-zone = "shopping";
+  shopping-zone = "Shopping";
 in
 {
   services.home-assistant.config = {
+    zone = [
+      {
+        name = shopping-zone;
+        latitude = "!secret shopping_latitude";
+        longitude = "!secret shopping_longitude";
+        radius = "63";
+      }
+    ];
     automation = [
       {
         alias = "Send shopping list when arrive at store";
         trigger = {
           platform = "zone";
           entity_id = devices.entity-id.phones.pixel.location;
-          zone = shopping-zone;
+          zone = "zone.${shopping-zone}";
           event = "enter";
         };
         action = {
