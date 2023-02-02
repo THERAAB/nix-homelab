d205c176680ea76499a40c6f47edea440b17f3cb
raab <rob.w.lago@gmail.com>
2023-02-02 11:29:55 -0500

    updating tailscale ip

--- configuration.nix
+++ configuration.nix
@@ -18,36 +18,6 @@
     };
   };
 
-  systemd.services = {
-    nixos-upgrade.onFailure = [ "nixos-upgrade-on-failure.service" ];
-    nixos-upgrade-on-failure = {
-      script = ''
-        TOKEN=`cat ${config.sops.secrets.pushbullet_api_key.path}`
-
-        echo '{"type":"file","title":"Nixos Upgrade Failed","body":"' > body.json
-        journalctl -n 15 -u nixos-upgrade.service  >> body.json
-        echo '"}' >> body.json
-
-        ${pkgs.curl}/bin/curl   -H "Access-Token: $TOKEN"               \
-                                -H "Content-Type: application/json"     \
-                                -X POST                                 \
-                                -d @body.json                           \
-                                https://api.pushbullet.com/v2/pushes
-      '';
-    };
-  };
-
-
-  system.autoUpgrade = {
-    enable = true;
-    dates = "04:00";
-    flake = "/nix/persist/nix-homelab";
-    flags = [
-      "--update-input" "nixpkgs"
-    ];
-    allowReboot = true;
-  };
-
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
---
+++ system/auto-upgrade.nix
@@ -0,0 +1,31 @@
+{ config, pkgs, ... }:
+{
+  systemd.services = {
+    nixos-upgrade.onFailure = [ "nixos-upgrade-on-failure.service" ];
+    nixos-upgrade-on-failure = {
+      script = ''
+        TOKEN=`cat ${config.sops.secrets.pushbullet_api_key.path}`
+
+        echo '{"type":"file","title":"Nixos Upgrade Failed","body":"' > body.json
+        journalctl -n 15 -u nixos-upgrade.service  >> body.json
+        echo '"}' >> body.json
+
+        ${pkgs.curl}/bin/curl   -H "Access-Token: $TOKEN"               \
+                                -H "Content-Type: application/json"     \
+                                -X POST                                 \
+                                -d @body.json                           \
+                                https://api.pushbullet.com/v2/pushes
+      '';
+    };
+  };
+
+  system.autoUpgrade = {
+    enable = true;
+    dates = "04:00";
+    flake = "/nix/persist/nix-homelab";
+    flags = [
+      "--update-input" "nixpkgs"
+    ];
+    allowReboot = true;
+  };
+}
