84d0154f32637dc54c698c1bab12662803d334f8
raab <rob.w.lago@gmail.com>
2023-02-02 11:24:17 -0500

    updating tailscale ip

--- configuration.nix
+++ configuration.nix
@@ -22,10 +22,9 @@
     nixos-upgrade.onFailure = [ "nixos-upgrade-on-failure.service" ];
     nixos-upgrade-on-failure = {
       script = ''
+        echo test > test.txt
         TOKEN=`cat ${config.sops.secrets.pushbullet_api_key.path}`
-        echo '{"type":"note","title":"Nixos Upgrade Failed","body":"' > body.json
-        journalctl -n 15 -u nixos-upgrade.service  >> body.json
-        echo '"}' >> body.json
+        echo '{"type":"file","title":"Nixos Upgrade Failed","file_name":"test.txt","file_type":"text/plain"}' > body.json
 
         ${pkgs.dos2unix}/bin/unix2dos body.json
         ${pkgs.curl}/bin/curl   -H "Access-Token: $TOKEN"               \
