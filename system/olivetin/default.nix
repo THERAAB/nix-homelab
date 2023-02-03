833a24d8e5772e55b3976636b291d77e17e60f70
raab <rob.w.lago@gmail.com>
2023-02-03 08:17:40 -0500

    updating tailscale ip

--- system/olivetin/default.nix
+++ system/olivetin/default.nix
@@ -35,9 +35,7 @@ in
     image = "jamesread/${app-name}";
     volumes = [
       "${local-config-dir}:/config"
-      "/var/run/docker.sock:/var/run/docker.sock"
     ];
-    #user = "root";
     user = "${toString uid}";
     ports = [ "${toString port}:${toString port}" ];
     environment = {
