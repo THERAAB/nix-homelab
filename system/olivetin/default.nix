06227210a82506af57638f3b38bb9ad99d314a64
raab <rob.w.lago@gmail.com>
2023-02-03 14:33:50 -0500

    updating tailscale ip

--- system/olivetin/default.nix
+++ system/olivetin/default.nix
@@ -60,7 +60,7 @@ in
     };
   };
   systemd.tmpfiles.rules = [
-    "r  ${www-dir}                    -           -               -               -   ${olivetin}/www "
+    "r  ${www-dir}                    -           -               -               -   -               "
     "L  ${www-dir}                    -           -               -               -   ${olivetin}/www "
     "r  ${scripts-dir}/commands.sh    -           -               -               -   -               "
     "L  ${scripts-dir}/commands.sh    -           -               -               -   ${shellScript}  "
