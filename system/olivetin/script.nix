a398ef4238c03e5fd3d5c97f682f2e400951ed0e
raab <rob.w.lago@gmail.com>
2023-02-04 10:09:49 -0500

    updating tailscale ip

--- system/olivetin/script.nix
+++ system/olivetin/script.nix
@@ -1,16 +1,43 @@
 { config, pkgs, ... }:
 
 pkgs.writeShellScript "commands.sh" ''
-  case "$1" in
-    reboot)
-      reboot now;;
-    jellyfin_reboot)
-      podman stop jellyfin
-      sleep 1
-      podman start jellyfin;;
-    gatus_reboot)
-      podman stop gatus
-      sleep 1
-      podman start gatus;;
-  esac
+  # OliveTin has nopasswd sudo access to this script, so we want to limit what it can do as root
+
+  function show_usage() {
+    printf "Usage: $0 [option] [parameter]]\n"
+    printf "\n"
+    printf "Options:\n"
+    printf " -p|--podman [container name], restart container\n"
+    printf " -r|--reboot, Reboot system\n"
+    printf " -h|--help, Print help\n"
+    return 0
+  }
+
+  function restart_podman_container() {
+    podman stop $1
+    sleep 1
+    podman start $1
+    return 0
+  }
+
+  # Check inputs
+  if [[ $# -gt 2 ]] || [[ $# -eq 0 ]];then
+    echo "Either 0 or more than 2 input arguments provided which is not supported"
+    show_usage
+    exit 1
+  fi
+
+  while [ ! -z "$1" ]; do
+    if [[ "$1" == "--help" ]] || [[ "$1" == "-h" ]]; then
+      show_usage
+    elif [[ "$1" == "-p" ]] || [[ "$1" == "--podman" ]]; then
+      restart_podman_container $2
+    elif [[ $1 == "-r" ]] || [[ "$1" == "--reboot" ]]; then
+      reboot now
+    else
+      echo "Incorrect input provided $1"
+      show_usage
+    fi
+  shift
+  done
 ''
