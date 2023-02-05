7d5d4bc0d66a51a4fb38dc528da139c40a774c71
raab <rob.w.lago@gmail.com>
2023-02-05 17:33:33 -0500

    updating tailscale ip

--- pkgs/bios/default.nix
+++ pkgs/bios/default.nix
@@ -7,6 +7,9 @@
       inherit pname version;
       hash = "sha256-vM/CQBG2pjGm7e7xBpVRpOyq/3s+1QpiIaaAdYUFAOk=";
     };
+
+    doCheck = false;
+
     propagatedBuildInputs = [
       oyaml pyyaml
     ];
