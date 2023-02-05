95b4bb6b8ceed1dee0eb7b3d8b85b31a5b906c51
raab <rob.w.lago@gmail.com>
2023-02-05 18:29:04 -0500

    updating tailscale ip

--- overlays/default.nix
+++ overlays/default.nix
@@ -10,17 +10,5 @@
     # example = prev.example.overrideAttrs (oldAttrs: rec {
     # ...
     # });
-    python = prev.python.override {
-      packageOverrides = final: prev: {
-        my_stuff = prev.buildPythonPacakge rec {
-          pname = "aiohttp";
-          version = "3.8.3";
-          src = prev.fetchPypi {
-            inherit pname version;
-            sha256 = "";
-          };
-        };
-      };
-    };
   };
 }
