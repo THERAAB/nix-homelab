56eddcb8cd146cf84f96af4e64e530a915c33677
raab <rob.w.lago@gmail.com>
2023-02-05 16:48:03 -0500

    updating tailscale ip

--- configuration.nix
+++ configuration.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ inputs, outputs, lib, config, pkgs, ... }:
 {
   imports = [ 
     ./hardware-configuration.nix
--- home.nix
+++ home.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ inputs, outputs, lib, config, pkgs, ... }:
 {
   imports = [
     ./home
