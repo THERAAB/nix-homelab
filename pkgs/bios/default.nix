a14a86ccb659f4270672f0c26432ff6c30c1f578
raab <rob.w.lago@gmail.com>
2023-02-05 18:48:28 -0500

    adding bios package

--- pkgs/bios/default.nix
+++ pkgs/bios/default.nix
@@ -13,4 +13,14 @@
     propagatedBuildInputs = [
       oyaml pyyaml
     ];
+
+    meta = with lib; {
+      description = '''bios' is a python library which helps you for operations of I/O.
+      You can read determined type of files and assign the content of the files to the best suitable data type for these contents.
+      If it is required, developer should handle the exception handling issues.'';
+      homepage = "https://github.com/bilgehannal/bios";
+      license = licenses.mit;
+      maintainers = with maintainers; [ bilgehannal ];
+    };
+
   }
