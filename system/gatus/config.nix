6b9eabe7432afd29b990c33e315161a740e7cf5b
raab <rob.w.lago@gmail.com>
2023-02-07 20:26:44 -0500

    updating tailscale ip

--- system/gatus/config.nix
+++ system/gatus/config.nix
@@ -45,7 +45,7 @@
     }
     {
       name = "Ring Doorbell";
-      url = "tcp://192.168.1.108:8557";
+      url = "tcp://192.168.1.108:7076";
       conditions = [
         "[CONNECTED] == true"
       ];
