328b720918e8e476ab0040da7598c52999308d34
raab <rob.w.lago@gmail.com>
2023-02-09 11:15:41 -0500

    updating static mappings

--- system/network.properties.nix
+++ system/network.properties.nix
@@ -8,9 +8,9 @@
     subnet = "192.168.2.0/24";
   };
   pfSense.local.ip = "192.168.1.1";
-  tplink.local.ip = "192.168.1.102";
+  tplink.local.ip = "192.168.1.2";
   govee-water-alarm.local.ip = "192.168.1.3";
-  ring-doorbell.local.ip = "192.168.1.108";
+  ring-doorbell.local.ip = "192.168.1.4";
   domain.local = "box";
   domain.tail = "tail";
 }
