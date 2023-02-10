8f19c8e159bbb5e83b7b70b47f635b8f4c3dd305
raab <rob.w.lago@gmail.com>
2023-02-10 09:03:41 -0500

    adding _blank

--- system/boot.nix
+++ system/boot.nix
@@ -4,7 +4,12 @@
     kernelPackages = pkgs.linuxPackages_latest;
     loader.systemd-boot.enable = true;
     loader.efi.canTouchEfiVariables = true;
-    # Force kernel to use the right drive & use graphics controller
+    # Force kernel to use the right driver & use graphics controller
     kernelParams = [ "i915.force_probe=4692" "i915.enable_guc=3" ];
   };
+
+  # Install r8168 driver (RealTek proprietary) for network controller
+  environment.systemPackages = [ pkgs.linuxKernel.packages.linux_6_1.r8168 ];
+  # and blacklist kernel module driver
+  blacklistedKernelModules = [ "r8169" ];
 }
