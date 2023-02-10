83b7509ec0f349b2585be6e408cf498d53f022ce
raab <rob.w.lago@gmail.com>
2023-02-10 09:20:11 -0500

    adding _blank

--- system/boot.nix
+++ system/boot.nix
@@ -7,9 +7,4 @@
     # Force kernel to use the right driver & use graphics controller
     kernelParams = [ "i915.force_probe=4692" "i915.enable_guc=3" ];
   };
-
-  # Install r8168 driver (RealTek proprietary) for network controller
-  # environment.systemPackages = [ pkgs.linuxKernel.packages.linux_6_1.r8168 ];
-  # and blacklist kernel module driver
-  # boot.blacklistedKernelModules = [ "r8169" ];
 }
