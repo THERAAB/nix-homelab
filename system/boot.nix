991c4e882fc33cde92d4169045ad81292d11866a
raab <rob.w.lago@gmail.com>
2023-02-11 16:17:25 -0500

    fixing ethernet driver

--- system/boot.nix
+++ system/boot.nix
@@ -6,5 +6,8 @@
     loader.efi.canTouchEfiVariables = true;
     # Force kernel to use the right driver & use graphics controller
     kernelParams = [ "i915.force_probe=4692" "i915.enable_guc=3" ];
+    # Blacklist r8169 Network driver because it sucks for RTL8125B
+    # This will probably load r8168 (Realtek driver) instead
+    blacklistedKernelModules = [ "r8169" ];
   };
 }
