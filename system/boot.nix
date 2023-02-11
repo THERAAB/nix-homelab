28d59ae83bd9ed3847a63dfaedb92fb2e3b3de53
raab <rob.w.lago@gmail.com>
2023-02-11 16:57:56 -0500

    fixing ethernet driver

--- system/boot.nix
+++ system/boot.nix
@@ -4,8 +4,7 @@
     kernelPackages = pkgs.linuxPackages_latest;
     loader.systemd-boot.enable = true;
     loader.efi.canTouchEfiVariables = true;
-    # Force kernel to use the right driver & use graphics controller
+    # Force kernel to use the right CPU driver & use graphics controller
     kernelParams = [ "i915.force_probe=4692" "i915.enable_guc=3" ];
-    extraModulePackages = [ pkgs.linuxKernel.packages.linux_6_1.r8125 ];
   };
 }
