b25576f23d2211231e231aad9172b1fe689b9b8a
raab <rob.w.lago@gmail.com>
2023-02-12 08:15:37 -0500

    fixing ethernet driver

--- pkgs/r8125/default.nix
+++ pkgs/r8125/default.nix
@@ -12,7 +12,7 @@ stdenv.mkDerivation rec {
     owner = "awesometic";
     repo = "realtek-r8125-dkms";
     rev = version;
-    sha256 = "";
+    sha256 = "sha256-b7TXUSdBYYACRvImK0ocfC/eWtdYN8hhwqybPZQbLAc=";
   };
 
   hardeningDisable = [ "pic" ];
