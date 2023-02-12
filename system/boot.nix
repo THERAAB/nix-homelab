971f77b253494a41797fdca990a753b37516e882
raab <rob.w.lago@gmail.com>
2023-02-12 12:51:56 -0500

    fixing ethernet driver

--- system/boot.nix
+++ system/boot.nix
@@ -11,19 +11,5 @@
       "vm.dirty_writeback_centisecs" = 6000;
       "vm.laptop_mode" = 5;
     };
-    # Adding r8168 kernel module for ethernet
-    extraModulePackages =  [ pkgs.r8168 ];
-    kernelModules = [ "r8168" ];
   };
-  # /sys/class/net/enp3s0
-  # sudo udevadm test-builtin net_id /sys/class/net/enp3s0 > /dev/null
-  # enp3s0: MAC address identifier: hw_addr=d8:5e:d3:96:1c:ac → xd85ed3961cac
-  # 03:00.0 Ethernet controller: Realtek Semiconductor Co., Ltd. RTL8125 2.5GbE Controller (rev 05)
-  # SUBSYSTEM=="net", ACTION=="add", ATTR{address}="d8:5e:d3:96:1c:ac",
-  # SUBSYSTEM=="net", ACTION=="add", KERNELS=="03:00.0"
-  # /run/booted-system/kernel-modules/lib/modules/5.15.92/kernel/drivers/net/ethernet/realtek/
-  # udevadm info -a -p /sys/class/net/enp3s0
-  services.udev.extraRules = ''
-    SUBSYSTEM=="net", KERNELS=="0000:03:00.0", ACTION=="add", DRIVERS=="r8168"
-  '';
 }
