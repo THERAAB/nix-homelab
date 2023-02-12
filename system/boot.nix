fc0d44e06f2a052f73442e723495375a0db26dd1
raab <rob.w.lago@gmail.com>
2023-02-12 07:51:00 -0500

    fixing ethernet driver

--- system/boot.nix
+++ system/boot.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, pkgs, stdenv, ... }:
 {  
   boot = {
     kernelPackages = pkgs.linuxPackages_latest;
@@ -9,7 +9,7 @@
     # Adding patched r8125 kernel module for ethernet
     extraModulePackages = [
       (
-        mkDerivation rec {
+        stdenv.mkDerivation rec {
           pname = "r8125";
           # On update please verify (using `diff -r`) that the source matches the
           # realtek version.
