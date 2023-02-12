4c7bef54aeab23d2f6fd35d82e89bef41753956b
raab <rob.w.lago@gmail.com>
2023-02-12 11:43:38 -0500

    fixing ethernet driver

--- system/boot.nix
+++ system/boot.nix
@@ -15,4 +15,13 @@
     extraModulePackages = with config.boot.kernelPackages; [ r8168 ];
     kernelModules = [ "r8168" ];
   };
+  # /sys/class/net/enp3s0
+  # sudo udevadm test-builtin net_id /sys/class/net/enp3s0 > /dev/null
+  # enp3s0: MAC address identifier: hw_addr=d8:5e:d3:96:1c:ac → xd85ed3961cac
+  # 03:00.0 Ethernet controller: Realtek Semiconductor Co., Ltd. RTL8125 2.5GbE Controller (rev 05)
+  # SUBSYSTEM=="net", ACTION=="add", ATTR{address}="d8:5e:d3:96:1c:ac",
+  # SUBSYSTEM=="net", ACTION=="add", KERNELS=="03:00.0"
+  services.udev.extraRules = ''
+    SUBSYSTEM=="net", ACTION=="add", ATTR{address}="d8:5e:d3:96:1c:ac", DRIVERS=="r8168"
+  '';
 }
