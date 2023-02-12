8d9555c2de42e36725533df7e84af4e1d1b259a2
raab <rob.w.lago@gmail.com>
2023-02-12 07:35:33 -0500

    fixing ethernet driver

--- system/boot.nix
+++ system/boot.nix
@@ -6,5 +6,6 @@
     loader.efi.canTouchEfiVariables = true;
     # Force kernel to use the right CPU driver & use graphics controller
     kernelParams = [ "i915.force_probe=4692" "i915.enable_guc=3" ];
+    extraModulePackages = [ pkgs.r8125 ];
   };
 }
