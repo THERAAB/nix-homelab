685462871c078994489f1370edd38d5c3136feaf
raab <rob.w.lago@gmail.com>
2023-02-12 15:10:44 -0500

    fixing ethernet driver

--- system/boot.nix
+++ system/boot.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
 {  
   boot = {
-    kernelPackages = pkgs.linuxPackages_5_15;
+    kernelPackages = pkgs.linuxPackages_latest;
     loader.systemd-boot.enable = true;
     loader.efi.canTouchEfiVariables = true;
     # Force kernel to use the right CPU driver & use graphics controller
