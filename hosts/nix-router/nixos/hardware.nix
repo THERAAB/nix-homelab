dd60dde91a1410e15e39f7eb32e198e46e6def74
raab <rob.w.lago@gmail.com>
2023-02-22 13:05:35 -0500

    updating dnsmasq

--- hosts/nix-router/nixos/hardware.nix
+++ hosts/nix-router/nixos/hardware.nix
@@ -60,7 +60,7 @@ in
       dhcp-range = [ "10.10.11.100,10.10.11.254,24h" "10.10.12.100,10.10.12.254,24h" "10.10.13.100,10.10.13.254,24h" ];
       dhcp-host = [
         "${network.nix-server.mac},${network.nix-server.local.ip}"
-        "${network.desktop.mac},${network.desktop.local.ip}" 
+        "${network.desktop.mac},${network.desktop.local.ip}"
       ];
     };
   };
