c555142b09ac1c3321392f38dc9a4758aca81dc1
raab <rob.w.lago@gmail.com>
2023-03-10 09:13:54 -0500

    fixing govee

--- share/lib/pkgs/hacs-govee/default.nix
+++ share/lib/pkgs/hacs-govee/default.nix
@@ -8,10 +8,11 @@
       repo = pname;
       rev = version;
       owner  = "LaggAt";
-      sha256 = "sha256-vIBx+t+AcWG9z7O5bv4yMMCplpc54N29/QxMUwHjeSU=";
+      sha256 = ""; #sha256-vIBx+t+AcWG9z7O5bv4yMMCplpc54N29/QxMUwHjeSU=
     };
     installPhase = ''
       mkdir -p $out/custom_components
       cp -r ./custom_components/govee $out/custom_components/govee
+      echo hello
     '';
   }
