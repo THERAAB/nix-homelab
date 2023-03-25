4f8d4d7e5aba43c707cc10794f8456b5d39c66e8
THERAAB <rob.w.lago@gmail.com>
2023-03-25 11:16:36 -0400

    update ips

--- share/network.properties.nix
+++ share/network.properties.nix
@@ -5,11 +5,11 @@
   };
   desktop.subnet = "192.168.2.0/24";
   pfSense.local.ip = "192.168.1.1";
-  tplink.local.ip = "192.168.0.1";
-  govee-water-alarm.local.ip = "192.168.0.4";
-  ring-doorbell.local.ip = "192.168.0.3";
-  kasa-living-room-plug.local.ip = "192.168.0.6";
-  android-tv.local.ip = "192.168.0.5";
+  tplink.local.ip = "192.168.1.2";
+  govee-water-alarm.local.ip = "192.168.1.4";
+  ring-doorbell.local.ip = "192.168.1.3";
+  kasa-living-room-plug.local.ip = "192.168.1.6";
+  android-tv.local.ip = "192.168.1.5";
   domain.local = "box";
   domain.tail = "tail";
 }
