1f9e335a70cd18af16f0167dc174f5e7f9f4626e
THERAAB <rob.w.lago@gmail.com>
2023-03-28 09:57:02 -0400

    updating fish

--- share/nixos/fish.nix
+++ share/nixos/fish.nix
@@ -36,6 +36,9 @@
         format = "at [$path]($style)[$read_only]($read_only_style) ";
         read_only = " ";
       };
+      git_branch = {
+        format = "in [$symbol$branch(:$remote_branch)]($style) ";
+      };
       character = {
         success_symbol = "[➜](bold green)";
         error_symbol = "[➜](bold red)";
