bd13f33f8031748804890f8db0c4db1f9f3d874a
THERAAB <rob.w.lago@gmail.com>
2023-04-04 00:47:09 -0400

    changing homer to catppuccin

--- hosts/nix-server/nixos/homer/settings-default.nix
+++ hosts/nix-server/nixos/homer/settings-default.nix
@@ -12,8 +12,8 @@
       highlight-hover = "#414559";
       background = "#303446";
       card-background = "#414559";
-      text = "#df8e1d";
-      text-header = "#c6d0f5";
+      text = "#c6d0f5";
+      text-header = "#df8e1d";
       text-title = "#c6d0f5";
       text-subtitle = "#f4b8e4";
       card-shadow = "rgba(0, 0, 0, 0.1)";
