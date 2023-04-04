fb586827a34fc0822531575b3a0349c0300cb852
THERAAB <rob.w.lago@gmail.com>
2023-04-04 09:38:31 -0400

    changing homer to catppuccin

--- share/home/helix.nix
+++ share/home/helix.nix
@@ -4,6 +4,17 @@
     enable = true;
     settings = {
       theme = "base16_default";
+      editor = {
+        line-number = "relative";
+        cursor-shape = {
+          insert = "bar";
+          normal = "block";
+          select = "underline";
+        };
+        file-picker = {
+          hidden = false;
+        };
+      };
     };
   };
 }
