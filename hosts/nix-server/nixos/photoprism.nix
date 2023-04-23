9f0b106d3eaf5e0c5e901b1ce5d0925188755704
THERAAB <rob.w.lago@gmail.com>
2023-04-23 07:52:33 -0400

    adding photoprism

--- hosts/nix-server/nixos/photoprism.nix
+++ hosts/nix-server/nixos/photoprism.nix
@@ -1,15 +1,24 @@
 { config, pkgs, ... }:
 let
   port = 2342;
+  app-name = photoprism;
 in
 {
-  services.photoprism = {
+  services.${app-name} = {
     enable = true;
     originalsPath = "/photos";
     address = "192.168.3.2";
     settings = {
       PHOTOPRISM_ADMIN_PASSWORD = "insecure";
     };
+  };
+  services.caddy.virtualHosts = {
+    "http://${app-name}.${network.domain.local}".extraConfig = ''
+      reverse_proxy http://127.0.0.1:${toString port}
+    '';
+    "http://${app-name}.${network.domain.tail}".extraConfig = ''
+      reverse_proxy http://127.0.0.1:${toString port}
+    '';
   };
   networking.firewall.allowedTCPPorts = [ port ];
 }
