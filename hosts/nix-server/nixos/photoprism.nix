3cfdcb19b9a481c4e6db30c31a0e994a7858994c
THERAAB <rob.w.lago@gmail.com>
2023-04-23 07:54:12 -0400

    adding photoprism

--- hosts/nix-server/nixos/photoprism.nix
+++ hosts/nix-server/nixos/photoprism.nix
@@ -8,7 +8,7 @@ in
   services.${app-name} = {
     enable = true;
     originalsPath = "/photos";
-    address = "192.168.3.2";
+    address = "${app-name}.${network.domain.local}";
     settings = {
       PHOTOPRISM_ADMIN_PASSWORD = "insecure";
     };
