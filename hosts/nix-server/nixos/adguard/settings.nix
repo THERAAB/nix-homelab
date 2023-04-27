6117f0aba6cd92f1e9b13b537a73f43d397eba83
THERAAB <rob.w.lago@gmail.com>
2023-04-27 11:37:39 -0400

    update adguard upstream dns

--- hosts/nix-server/nixos/adguard/settings.nix
+++ hosts/nix-server/nixos/adguard/settings.nix
@@ -16,8 +16,8 @@ in
       ratelimit = 0;
       bind_hosts = [ "0.0.0.0" ];
       port = 53;
-      # TODO: FIX upstream_dns = [ "${network.pfSense.local.ip}" ];
-      upstream_dns = [ "1.1.1.1" ];
+      upstream_dns = [ "${network.pfSense.local.ip}" ];
+      # upstream_dns = [ "1.1.1.1" ];
       protection_enabled = true;
       blocked_hosts = [ "version.bind" "id.server" "hostname.bind" ];
       trusted_proxies = [ "127.0.0.0/8" "::1/128" ];
