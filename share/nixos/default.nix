ea111af4573816d100f8385dbe8e0746d6e2e300
THERAAB <rob.w.lago@gmail.com>
2023-04-30 09:45:45 -0400

    remove netavark hack

--- share/nixos/default.nix
+++ share/nixos/default.nix
@@ -1,7 +1,6 @@
 { config, pkgs, ... }:
 {
   imports = [
-    # ./podman.nix
     ./persist.nix
     ./system.nix
     ./pkgs.nix
--- share/nixos/podman.nix
+++
@@ -1,8 +0,0 @@
-{ config, pkgs, ... }:
-{
-  # Adding netavark to helper binaries so podman health check stops failing
-  virtualisation = {
-    podman.extraPackages = [ pkgs.netavark ];
-    containers.containersConf.settings.engine.helper_binaries_dir = ["${pkgs.netavark}/bin"];
-  };
-}
