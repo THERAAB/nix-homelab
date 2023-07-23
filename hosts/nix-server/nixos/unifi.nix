bd684098d11fd3aa81d38de55114f9dc4939a610
THERAAB <rob.w.lago@gmail.com>
2023-07-23 09:26:19 -0400

    adding unifi controller

--- hosts/nix-server/nixos/unifi.nix
+++ hosts/nix-server/nixos/unifi.nix
@@ -1,28 +1,31 @@
 {...}: let
   uid = 8112;
+  gid = 8111;
   port = 8443;
   app-name = "unifi-controller";
-  # network = import ../../../../share/network.properties.nix;
+  network = import ../../../../share/network.properties.nix;
   local-config-dir = "/nix/persist/${app-name}/";
 in {
   users = {
     users."${app-name}" = {
       uid = uid;
+      group = app-name;
       isSystemUser = true;
     };
+    groups.${app-name}.gid = gid;
   };
   systemd.tmpfiles.rules = [
     "d    ${local-config-dir}     -       -             -        -   - "
     "Z    ${local-config-dir}     740     ${app-name}   -        -   - "
   ];
-  #services.caddy.virtualHosts = {
-  #  "http://${app-name}.${network.domain.local}".extraConfig = ''
-  #    reverse_proxy http://127.0.0.1:${toString port}
-  #  '';
-  #  "http://${app-name}.${network.domain.tail}".extraConfig = ''
-  #    reverse_proxy http://127.0.0.1:${toString port}
-  #  '';
-  #};
+  services.caddy.virtualHosts = {
+    "http://${app-name}.${network.domain.local}".extraConfig = ''
+      reverse_proxy http://127.0.0.1:${toString port}
+    '';
+    "http://${app-name}.${network.domain.tail}".extraConfig = ''
+      reverse_proxy http://127.0.0.1:${toString port}
+    '';
+  };
   virtualisation.oci-containers.containers."${app-name}" = {
     autoStart = true;
     image = "linuxserver/${app-name}";
