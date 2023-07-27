444a789dbd2cb70dc0019d36866a1352d401da91
THERAAB <rob.w.lago@gmail.com>
2023-07-27 16:38:58 -0400

    adding unifi controller

--- hosts/nix-server/nixos/caddy.nix
+++ hosts/nix-server/nixos/caddy.nix
@@ -3,13 +3,5 @@
 in {
   services.caddy = {
     enable = true;
-    virtualHosts = {
-      "http://tplink.${network.domain.local}".extraConfig = ''
-        reverse_proxy http://${network.tplink.local.ip}
-      '';
-      "http://tplink.${network.domain.tail}".extraConfig = ''
-        reverse_proxy http://${network.tplink.local.ip}
-      '';
-    };
   };
 }
