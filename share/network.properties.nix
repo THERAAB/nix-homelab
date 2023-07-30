104e400cf78aa59fe7a6faf3c0f42a9e2c5ace84
THERAAB <rob.w.lago@gmail.com>
2023-07-30 12:43:09 -0400

    update unifi

--- share/network.properties.nix
+++ share/network.properties.nix
@@ -5,12 +5,12 @@
   };
   desktop.subnet = "192.168.2.0/24";
   pfSense.local.ip = "192.168.1.1";
-  unifi-u6-plus.local.ip = "192.168.1.52";
-  unifi-usw-lite-8.local.ip = "192.168.1.51";
-  govee-water-alarm.local.ip = "192.168.1.3";
-  ring-doorbell.local.ip = "192.168.1.4";
-  kasa-living-room-plug.local.ip = "192.168.1.6";
-  android-tv.local.ip = "192.168.1.5";
+  unifi-u6-plus.local.ip = "192.168.1.2";
+  unifi-usw-lite-8.local.ip = "192.168.1.3";
+  govee-water-alarm.local.ip = "192.168.127.3";
+  ring-doorbell.local.ip = "192.168.127.4";
+  kasa-living-room-plug.local.ip = "192.168.127.6";
+  android-tv.local.ip = "192.168.127.5";
   domain.local = "box";
   domain.tail = "tail";
 }
