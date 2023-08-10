f9260ae7d09a104b06718070d34db994c8ee47d5
THERAAB <rob.w.lago@gmail.com>
2023-08-10 19:39:31 -0400

    tailscale magic

--- hosts/nix-server/nixos/adguard/default.nix
+++ hosts/nix-server/nixos/adguard/default.nix
@@ -7,7 +7,7 @@ in {
   services.yamlConfigMaker.gatus.settings.endpoints = [
     {
       name = "Adguard";
-      url = "http://${app-name}.${network.domain.box}/";
+      url = "http://${app-name}.${network.domain.local}/";
       conditions = [
         "[STATUS] == 200"
         ''[BODY] == pat(*<title>Login</title>*)''
