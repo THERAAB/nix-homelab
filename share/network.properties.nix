3697d29d837ec13b6d388f428c6137cff63bb7f9
THERAAB <rob.w.lago@gmail.com>
2023-08-10 20:51:34 -0400

    tailscale magic

--- share/network.properties.nix
+++ share/network.properties.nix
@@ -12,6 +12,6 @@
   kasa-living-room-plug.local.ip = "192.168.127.6";
   android-tv.local.ip = "192.168.127.5";
   b-hyve.local.ip = "192.168.127.7";
-  domain.tail = "tail";
+  domain.tail = "tail.box";
   domain.box = "box";
 }
