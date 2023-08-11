b7f27aa7afba576dd0842b771fdc4314306d0152
THERAAB <rob.w.lago@gmail.com>
2023-08-11 14:17:59 -0400

    tailscale magic

--- hosts/nix-server/nixos/adguard/settings.nix
+++ hosts/nix-server/nixos/adguard/settings.nix
@@ -25,14 +25,6 @@ in {
       bootstrap_dns = ["9.9.9.0" "149.112.112.10" "2620:fe::10" "2620:fe::fe:10"];
       rewrites = [
         {
-          domain = "*.${network.domain.local}";
-          answer = "${network.nix-server.local.ip}";
-        }
-        {
-          domain = "*.${network.domain.tail}";
-          answer = "${network.nix-server.tailscale.ip}";
-        }
-        {
           domain = "jellyfin.box.tail";
           answer = "${network.nix-server.tailscale.ip}";
         }
