8b8837ff7f23b5e2ed109711c0688ad036610e07
THERAAB <rob.w.lago@gmail.com>
2023-08-26 18:08:24 -0400

    add tapo light strip

--- hosts/nix-server/nixos/home-assistant/tapo-light-strip.nix
+++ hosts/nix-server/nixos/home-assistant/tapo-light-strip.nix
@@ -3,4 +3,22 @@
     "R  /var/lib/hass/custom_components/tapo   -       -       -       -   -                                                        "
     "L  /var/lib/hass/custom_components/tapo   770     hass    hass    -   ${pkgs.home-assistant-tapo-p100}/custom_components/tapo  "
   ];
+  services.home-assistant = {
+    extraPackages = python310Packages:
+      with python310Packages; [
+        (
+          buildPythonPackage rec {
+            pname = "plugp100";
+            version = "3.9.0";
+            src = fetchPypi {
+              inherit version pname;
+              hash = "";
+            };
+
+            propagatedBuildInputs = [
+            ];
+          }
+        )
+      ];
+  };
 }
