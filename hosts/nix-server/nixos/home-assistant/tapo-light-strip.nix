7b35b360f1b6f86b9f4acb34e51f31dff5682c64
THERAAB <rob.w.lago@gmail.com>
2023-08-27 20:21:58 -0400

    add motion automation

--- hosts/nix-server/nixos/home-assistant/tapo-light-strip.nix
+++ hosts/nix-server/nixos/home-assistant/tapo-light-strip.nix
@@ -38,7 +38,7 @@
         condition = {
           condition = "numeric_state";
           entity_id = "sensor.lumi_lumi_motion_ac02_illuminance";
-          below = "7";
+          below = "9";
         };
         action = [
           {
