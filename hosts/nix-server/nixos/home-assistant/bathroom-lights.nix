6806bf9b69cad8e17496c0078a6e54a3f07d0908
THERAAB <rob.w.lago@gmail.com>
2023-08-30 15:31:09 -0400

    add bathroom light automation

--- hosts/nix-server/nixos/home-assistant/bathroom-lights.nix
+++ hosts/nix-server/nixos/home-assistant/bathroom-lights.nix
@@ -1,74 +1,42 @@
 {...}: {
   services.home-assistant.config.automation = [
     {
-      alias = "Turn on dim Kitchen lights when motion detected at night";
+      alias = "Turn on Bathroom lights when motion detected";
       trigger = {
         platform = "state";
         entity_id = "binary_sensor.lumi_lumi_motion_ac02_motion_2";
         from = "off";
         to = "on";
       };
-      condition = {
-        condition = "sun";
-        after = "sunset";
-      };
       action = [
         {
-          service = "light.turn_on";
-          data = {
-            brightness_pct = 15;
-            color_temp = "400";
+          "if" = {
+            condition = "time";
+            before = "6:00:00";
+            after = "20:00:00";
           };
-          target.entity_id = [
-            "light.wiz_tunable_white_df9586"
-            "light.wiz_tunable_white_1ade1e"
-          ];
-        }
-        {
-          wait_for_trigger = {
-            platform = "state";
-            entity_id = "binary_sensor.lumi_lumi_motion_ac02_motion_2";
-            from = "on";
-            to = "off";
-            for = {
-              hours = 0;
-              minutes = 5;
-              seconds = 0;
+          "then" = {
+            service = "light.turn_on";
+            data = {
+              brightness_pct = 15;
+              color_temp = "400";
             };
+            target.entity_id = [
+              "light.wiz_tunable_white_df9586"
+              "light.wiz_tunable_white_1ade1e"
+            ];
           };
-        }
-        {
-          service = "light.turn_off";
-          target.entity_id = [
-            "light.wiz_tunable_white_df9586"
-            "light.wiz_tunable_white_1ade1e"
-          ];
-        }
-      ];
-    }
-    {
-      alias = "Turn on bright Kitchen lights when motion detected at day";
-      trigger = {
-        platform = "state";
-        entity_id = "binary_sensor.lumi_lumi_motion_ac02_motion_2";
-        from = "off";
-        to = "on";
-      };
-      condition = {
-        condition = "sun";
-        before = "sunset";
-      };
-      action = [
-        {
-          service = "light.turn_on";
-          data = {
-            brightness_pct = 100;
-            color_temp = "100";
+          "else" = {
+            service = "light.turn_on";
+            data = {
+              brightness_pct = 100;
+              color_temp = "100";
+            };
+            target.entity_id = [
+              "light.wiz_tunable_white_df9586"
+              "light.wiz_tunable_white_1ade1e"
+            ];
           };
-          target.entity_id = [
-            "light.wiz_tunable_white_df9586"
-            "light.wiz_tunable_white_1ade1e"
-          ];
         }
         {
           wait_for_trigger = {
