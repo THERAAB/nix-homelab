77b6b5f41408fb38e5aef0885821af1e9133b513
THERAAB <rob.w.lago@gmail.com>
2023-08-30 15:19:34 -0400

    add bathroom light automation

--- hosts/nix-server/nixos/home-assistant/bathroom-lights.nix
+++ hosts/nix-server/nixos/home-assistant/bathroom-lights.nix
@@ -46,5 +46,51 @@
         }
       ];
     }
+    {
+      alias = "Turn on bright Kitchen lights when motion detected at day";
+      trigger = {
+        platform = "state";
+        entity_id = "binary_sensor.lumi_lumi_motion_ac02_motion_2";
+        from = "off";
+        to = "on";
+      };
+      condition = {
+        condition = "sun";
+        before = "sunset";
+      };
+      action = [
+        {
+          service = "light.turn_on";
+          data = {
+            brightness_pct = 100;
+            color_temp = "100";
+          };
+          target.entity_id = [
+            "light.wiz_tunable_white_df9586"
+            "light.wiz_tunable_white_1ade1e"
+          ];
+        }
+        {
+          wait_for_trigger = {
+            platform = "state";
+            entity_id = "binary_sensor.lumi_lumi_motion_ac02_motion_2";
+            from = "on";
+            to = "off";
+            for = {
+              hours = 0;
+              minutes = 5;
+              seconds = 0;
+            };
+          };
+        }
+        {
+          service = "light.turn_off";
+          target.entity_id = [
+            "light.wiz_tunable_white_df9586"
+            "light.wiz_tunable_white_1ade1e"
+          ];
+        }
+      ];
+    }
   ];
 }
