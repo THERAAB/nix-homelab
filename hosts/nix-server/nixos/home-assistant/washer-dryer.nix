e51b2507a46dc4d28690d96ad6c9cea5547992cf
THERAAB <rob.w.lago@gmail.com>
2023-09-03 09:31:50 -0400

    add washing machine automation

--- hosts/nix-server/nixos/home-assistant/washer-dryer.nix
+++ hosts/nix-server/nixos/home-assistant/washer-dryer.nix
@@ -24,5 +24,29 @@
         }
       ];
     }
+    {
+      alias = "Dryer Done Notification";
+      trigger = {
+        platform = "numeric_state";
+        entity_id = "sensor.dryer_plug_active_power";
+        for = {
+          seconds = 90;
+        };
+        below = 5;
+      };
+      action = [
+        {
+          service = "notify.pushbullet";
+          data = {
+            message = "Dryer Done";
+            title = "Dryer Cycle finished";
+          };
+        }
+        {
+          service = "notify.notify";
+          data_template.message = "Dryer Done";
+        }
+      ];
+    }
   ];
 }
