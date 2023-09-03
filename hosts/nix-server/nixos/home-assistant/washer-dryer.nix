b779f9dc62fa5708380a89fd9d28c3f0c05defa9
THERAAB <rob.w.lago@gmail.com>
2023-09-03 09:22:06 -0400

    add washing machine automation

---
+++ hosts/nix-server/nixos/home-assistant/washer-dryer.nix
@@ -0,0 +1,27 @@
+{...}: {
+  services.home-assistant.config.automation = [
+    {
+      alias = "Washing Machine Done Notification";
+      trigger = {
+        platform = "numeric_state";
+        entity_id = "sensor.washing_machine_plug_active_power";
+        for = {
+          seconds = 90;
+        };
+      };
+      actions = [
+        {
+          service = "notify.pushbullet";
+          data = {
+            message = "Washing Machine Done";
+            title = "Washing Machine Cycle finished";
+          };
+        }
+        {
+          service = "notify.notify";
+          data_template.message = "Washing Machine Done";
+        }
+      ];
+    }
+  ];
+}
