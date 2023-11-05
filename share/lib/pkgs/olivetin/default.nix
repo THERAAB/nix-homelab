a21ed548eda08bdf95010f5ca287fcc9d603a560
THERAAB <rob.w.lago@gmail.com>
2023-11-05 08:45:28 -0500

    update olivetin

--- share/lib/pkgs/olivetin/default.nix
+++ share/lib/pkgs/olivetin/default.nix
@@ -7,7 +7,7 @@ stdenv.mkDerivation rec {
   version = "2023.10.25";
   src = fetchurl {
     url = "https://github.com/OliveTin/OliveTin/releases/download/${version}/OliveTin-linux-amd64.tar.gz";
-    sha256 = "";
+    sha256 = "sha256-kIlZlBIdv8eSImP2pG+1ox/H9k+2rWqmlQnzuG1T31M=";
   };
   installPhase = ''
     install -m755 -D ./OliveTin $out/bin/olivetin
