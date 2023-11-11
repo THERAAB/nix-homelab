64805da20f6c9da52d0ec8358abefa65994849e8
THERAAB <rob.w.lago@gmail.com>
2023-11-11 06:25:42 -0500

    podman auto-update

--- hosts/nix-server/nixos/podman.nix
+++ hosts/nix-server/nixos/podman.nix
@@ -1,6 +1,13 @@
-{...}: {
+{pkgs, ...}: {
   virtualisation.podman.autoPrune = {
     enable = true;
     flags = ["--all"];
+  };
+  systemd.services.podman-auto-update = {
+    script = ''
+      ${pkgs.podman}/bin/podman auto-update
+    '';
+    after = ["podman.service"];
+    requires = ["podman.service"];
   };
 }
