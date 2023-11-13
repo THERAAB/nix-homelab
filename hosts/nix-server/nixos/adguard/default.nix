84ef4bbdd1eaa8286852984f1cc3bd4271291921
THERAAB <rob.w.lago@gmail.com>
2023-11-13 10:37:57 -0500

    acme

--- hosts/nix-server/nixos/adguard/default.nix
+++ hosts/nix-server/nixos/adguard/default.nix
@@ -7,7 +7,7 @@ in {
   services.yamlConfigMaker.gatus.settings.endpoints = [
     {
       name = "Adguard";
-      url = "http://${app-name}.${network.domain}/";
+      url = "https://${app-name}.${network.domain}/";
       conditions = [
         "[STATUS] == 200"
         ''[BODY] == pat(*<title>Login</title>*)''
