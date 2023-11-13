baae67ffced8e678690f54dd36c4471dba5cbe6c
THERAAB <rob.w.lago@gmail.com>
2023-11-13 12:36:36 -0500

    unifi

--- hosts/nix-server/nixos/unifi.nix
+++ hosts/nix-server/nixos/unifi.nix
@@ -1,9 +1,6 @@
-{...}: let
-  uid = 7812;
-  gid = 7813;
+{pkgs, ...}: let
   port = 8443;
-  app-name = "unifi-controller";
-  local-config-dir = "/nix/persist/${app-name}/";
+  app-name = "unifi";
   network = import ../../../share/network.properties.nix;
 in {
   services.yamlConfigMaker.gatus.settings.endpoints = [
@@ -25,21 +22,9 @@ in {
     {
       title = "Restart Unifi";
       icon = ''<img src = "customIcons/unifi.png" width = "48px"/>'';
-      shell = "sudo /nix/persist/olivetin/scripts/commands.sh -p ${app-name}";
+      shell = "sudo /nix/persist/olivetin/scripts/commands.sh -s ${app-name}";
       timeout = 20;
     }
-  ];
-  users = {
-    users."${app-name}" = {
-      uid = uid;
-      group = app-name;
-      isSystemUser = true;
-    };
-    groups.${app-name}.gid = gid;
-  };
-  systemd.tmpfiles.rules = [
-    "d    ${local-config-dir}     -       -             -        -   - "
-    "Z    ${local-config-dir}     740     ${app-name}   -        -   - "
   ];
   services.caddy.virtualHosts."${app-name}.${network.domain}" = {
     useACMEHost = "${network.domain}";
@@ -51,23 +36,9 @@ in {
       }
     '';
   };
-  virtualisation.oci-containers.containers."${app-name}" = {
-    autoStart = true;
-    image = "lscr.io/linuxserver/${app-name}";
-    volumes = [
-      "${local-config-dir}:/config"
-    ];
-    ports = [
-      "${toString port}:${toString port}"
-      "3478:3478/udp"
-      "1001:1001/udp"
-      "8080:8080"
-    ];
-    environment = {
-      PUID = "${toString uid}";
-      PGID = "${toString gid}";
-      UMASK = "022";
-      TZ = "America/New_York";
-    };
+  networking.firewall.allowedTCPPorts = [port];
+  services.unifi = {
+    enable = true;
+    unifiPackage = pkgs.unifi;
   };
 }
