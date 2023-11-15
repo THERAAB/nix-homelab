a05b3afb45bf1fe55d32ffd112dd90d3ed4f58f2
THERAAB <rob.w.lago@gmail.com>
2023-11-14 21:48:33 -0500

    fish

--- share/home/fish.nix
+++ share/home/fish.nix
@@ -1,3 +1,24 @@
 {...}: {
-  programs.fish.enable = true;
+  programs = {
+    fish = {
+      enable = true;
+      shellAliases = {
+        nox = "/nix/persist/nix-dotfiles/nox";
+        cat = "bat --theme=base16-256";
+        grep = "rg";
+        ps = "procs";
+        du = "dust";
+        htop = "btm";
+      };
+    };
+    zoxide = {
+      enable = true;
+      options = ["--cmd cd"];
+    };
+    atuin.enable = true;
+    exa = {
+      enable = true;
+      enableAliases = true;
+    };
+  };
 }
--- share/nixos/fish.nix
+++ share/nixos/fish.nix
@@ -5,17 +5,6 @@
     enable = true;
     interactiveShellInit = ''
       set fish_greeting # Disable greeting
-      zoxide init --cmd cd fish | source
     '';
-    shellAliases = {
-      nox = "/nix/persist/nix-homelab/nox";
-      ls = "exa";
-      la = "exa -lah";
-      cat = "bat --theme=base16-256";
-      grep = "rg";
-      ps = "procs";
-      du = "dust";
-      htop = "btm";
-    };
   };
 }
