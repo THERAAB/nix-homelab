79881eb84591cb83edb718f0083c84fc87841325
THERAAB <rob.w.lago@gmail.com>
2023-11-14 20:17:58 -0500

    cloudflare dmz

--- hosts/nix-server/nixos/cloudflared.nix
+++ hosts/nix-server/nixos/cloudflared.nix
@@ -40,7 +40,7 @@ in {
       driver = "macvlan";
       id = "1123000000000000000000000000000000000000000000000000000000000000";
       internal = false;
-      ipam_options.driver = "dchp";
+      ipam_options.driver = "dhcp";
       ipv6_enabled = false;
       name = "dmz-macvlan";
       network_interface = "enp3s0";
