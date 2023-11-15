5938bc5326f6fd50059225d2525cd7f8c27391f3
THERAAB <rob.w.lago@gmail.com>
2023-11-14 20:03:43 -0500

    cloudflare dmz

--- hosts/nix-server/nixos/docker.nix
+++ hosts/nix-server/nixos/docker.nix
@@ -1,3 +1,27 @@
-{...}: {
-  virtualisation.docker.enable = true;
+{pkgs,...}: {
+  virtualisation.docker = {
+    enable = true;
+    storageDriver = "btrfs";
+    enableOnBoot = true;
+    autoPrune = {
+      enable = true;
+      flags = ["--all"];
+    };
+  };
+  systemd.services.docker-auto-update = {
+    script = ''
+      ${pkgs.docker}/bin/podman auto-update
+    '';
+    after = ["docker.service"];
+    requires = ["docker.service"];
+  };
+  systemd.timers.docker-auto-update = {
+    wantedBy = ["timers.target"];
+    timerConfig = {
+      OnCalendar = "weekly";
+      Persistent = "true";
+      Unit = "docker-auto-update.service";
+    };
+  };
+}
 }
