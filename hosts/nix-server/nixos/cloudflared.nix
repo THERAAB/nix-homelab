69f7dd65678358c16d7a6090aaa288a1d1622611
THERAAB <rob.w.lago@gmail.com>
2023-11-15 23:00:09 -0500

    cloudflare

--- hosts/nix-server/nixos/cloudflared.nix
+++ hosts/nix-server/nixos/cloudflared.nix
@@ -1,8 +1,9 @@
-{...}: let
+{pkgs, ...}: let
   uid = 999;
   gid = 999;
   local-config-dir = "/nix/persist/cloudflared";
   app-name = "cloudflared";
+  json = pkgs.formats.json {};
 in {
   users = {
     users."${app-name}" = {
@@ -27,7 +28,26 @@ in {
       "run"
     ];
     extraOptions = [
+      "--network=cloudflare-network"
       "-l=io.containers.autoupdate=registry"
     ];
+  };
+  environment.etc."containers/networks/cloudflare-network.json" = {
+    source = json.generate "cloudflare-network.json" {
+      dns_enabled = true;
+      driver = "bridge";
+      id = "4f72ec37e6860f72e48285f65f3e1bad7e5933cb939426e4ad6874200339353a";
+      internal = false;
+      ipam_options.driver = "host-local";
+      ipv6_enabled = false;
+      name = "cloudflare-network";
+      network_interface = "podman3";
+      subnets = [
+        {
+          gateway = "10.94.0.1";
+          subnet = "10.94.0.0/24";
+        }
+      ];
+    };
   };
 }
