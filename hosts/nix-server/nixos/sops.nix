1a0dbba53c30a50b282a254d14701e44304425cb
THERAAB <rob.w.lago@gmail.com>
2023-11-17 12:49:53 -0500

    add authelia

--- hosts/nix-server/nixos/sops.nix
+++ hosts/nix-server/nixos/sops.nix
@@ -8,12 +8,8 @@
       owner = "vuetorrent";
     };
     df_password = {};
-    authelia_jwt_secret = {
-      owner = "authelia";
-    };
-    authelia_storage_secret = {
-      owner = "authelia";
-    };
+    authelia_jwt_secret = {};
+    authelia_storage_secret = {};
     cloudflare_secret = {
       owner = "cloudflared";
       path = "/var/lib/secrets/cloudflare.secret";
