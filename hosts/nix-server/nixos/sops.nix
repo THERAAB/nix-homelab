52ff8c5f64e19880b2a96feb985ed5ab7177fb99
THERAAB <rob.w.lago@gmail.com>
2023-11-17 12:03:32 -0500

    update sops

--- hosts/nix-server/nixos/sops.nix
+++ hosts/nix-server/nixos/sops.nix
@@ -8,5 +8,17 @@
       owner = "vuetorrent";
     };
     df_password = {};
+    cloudflare_secret = {
+      owner = "cloudflared";
+      path = "/var/lib/secrets/cloudflare.secret";
+    };
+    mongo_init = {
+      owner = "unifi";
+      path = "/nix/persist/unifi/init-mongo.js";
+    };
+    mongo_secret = {
+      owner = "unifi";
+      path = "/nix/persist/unifi/env.secret";
+    };
   };
 }
