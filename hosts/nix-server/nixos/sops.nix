57f2051a93765b601c3aa924946262d6ff61d0b5
THERAAB <rob.w.lago@gmail.com>
2023-11-17 20:40:00 -0500

    refactor to /var/lib

--- hosts/nix-server/nixos/sops.nix
+++ hosts/nix-server/nixos/sops.nix
@@ -7,7 +7,7 @@
     wireguard_mullvad = {
       owner = "vuetorrent";
     };
-    df_password.owner = "photoprism";
+    df_password = {};
     authelia_jwt_secret.owner = "authelia-pumpkin-rodeo";
     authelia_storage_secret.owner = "authelia-pumpkin-rodeo";
     cloudflare_secret.owner = "cloudflared";
