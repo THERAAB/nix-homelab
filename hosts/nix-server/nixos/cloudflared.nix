d976e43f9da9ee3a1f17afee0e041f5cf1e66c69
THERAAB <rob.w.lago@gmail.com>
2023-11-17 20:55:47 -0500

    refactor to /var/lib

--- hosts/nix-server/nixos/cloudflared.nix
+++ hosts/nix-server/nixos/cloudflared.nix
@@ -21,11 +21,12 @@ in {
     image = "docker.io/cloudflare/${app-name}";
     user = "${toString uid}";
     environmentFiles = [
-      config.sops.secrets.cloudflare_secret.path
+      "${toString config.sops.secrets.cloudflare_secret.path}"
     ];
     cmd = [
       "tunnel"
       "run"
+      # "homepage"
     ];
     extraOptions = [
       "--network=cloudflare-network"
