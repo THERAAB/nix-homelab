5b4e833f8bae75bff17c0531c05d503c9252f38f
THERAAB <rob.w.lago@gmail.com>
2023-11-19 08:11:22 -0500

    add authelia

--- hosts/nix-server/nixos/authelia.nix
+++ hosts/nix-server/nixos/authelia.nix
@@ -49,7 +49,7 @@ in {
               "192.168.1.1/24" # AP Lan
               "192.168.127.5/32" # Android TV (for Jellyfin)
               "10.88.0.1/16" # Podman
-              "100.64.0.0/10" # Tailscale
+              # "100.64.0.0/10" # Tailscale
             ];
           }
         ];
