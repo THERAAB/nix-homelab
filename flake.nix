6c0f9da8ee994363b3bf026818fe19bb214451b0
THERAAB <rob.w.lago@gmail.com>
2023-11-23 09:46:31 -0500

    Revert "update to 23.11"
    This reverts commit 6d09b38375ad1ea5bc6ef89a09313341d67959d2.

--- flake.nix
+++ flake.nix
@@ -1,9 +1,9 @@
 {
   description = "nix-homelab flake";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/release-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
     home-manager = {
-      url = "github:nix-community/home-manager/master";
+      url = "github:nix-community/home-manager/release-23.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     impermanence.url = "github:nix-community/impermanence";
