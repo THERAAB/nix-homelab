80c7573faa453f28e12b40fd9b1ab741b0bee4bc
THERAAB <rob.w.lago@gmail.com>
2023-11-26 07:58:56 -0500

    add syncthing

--- hosts/nix-server/nixos/syncthing.nix
+++ hosts/nix-server/nixos/syncthing.nix
@@ -1,4 +1,37 @@
-{...}: {
+{...}: let
+  app-name = "syncthing";
+  network = import ../../../share/network.properties.nix;
+  port = 8384;
+in {
+  services.yamlConfigMaker.gatus.settings.endpoints = [
+    {
+      name = "SyncThing";
+      url = "https://${app-name}.${network.domain}/";
+      conditions = [
+        "[STATUS] == 200"
+      ];
+      alerts = [
+        {
+          type = "custom";
+        }
+      ];
+    }
+  ];
+  services.olivetin.settings.actions = [
+    {
+      title = "Restart SyncThing";
+      icon = ''<img src = "customIcons/${app-name}.png" width = "48px"/>'';
+      shell = "sudo /var/lib/olivetin/scripts/commands.sh -s ${app-name}";
+      timeout = 20;
+    }
+  ];
+  services.caddy.virtualHosts."${app-name}.${network.domain}" = {
+    useACMEHost = "${network.domain}";
+    extraConfig = ''
+      encode zstd gzip
+      reverse_proxy 127.0.0.1:${toString port}
+    '';
+  };
   services.syncthing = {
     enable = true;
     user = "raab";
