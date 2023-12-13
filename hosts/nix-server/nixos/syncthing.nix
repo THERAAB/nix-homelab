127f6722efe966db4a6c59053688a2552d620230
THERAAB <rob.w.lago@gmail.com>
2023-12-13 16:27:28 -0500

    update syncthing

--- hosts/nix-server/nixos/syncthing.nix
+++ hosts/nix-server/nixos/syncthing.nix
@@ -57,14 +57,13 @@ in {
           id = "YEUHTJT-HKSDRRS-FPPJCUU-ZWHQJTR-ZRP3LVM-BYFNSH7-MJ7BGPJ-C6PMFA6";
           addresses = ["tcp://${network.nix-desktop.tailscale.ip}:22000" "tcp://${network.nix-desktop.local.ip}:22000"];
         };
-        galaxy-s7-tab = {
-          id = "STQ62IM-HAMN7JJ-AXKOFPA-MLQC73I-KFOEPI4-MBMS44D-VWTFFAF-WAWNSQ3";
-        };
+        galaxy-s7-tab.id = "STQ62IM-HAMN7JJ-AXKOFPA-MLQC73I-KFOEPI4-MBMS44D-VWTFFAF-WAWNSQ3";
+        pixel-6a.id = "MCGDVOM-VJQ3IHA-HHLCELL-ABFIJT7-BFHHWMX-V77WXIF-OTEVLZH-F76I5Q6";
       };
       folders = {
         "${local-dir}/share" = {
           id = "share";
-          devices = ["nix-zenbook" "nix-desktop" "galaxy-s7-tab"];
+          devices = ["nix-zenbook" "nix-desktop" "galaxy-s7-tab" "pixel-6a"];
           versioning = {
             type = "staggered";
             params.maxAge = "7776000"; # 90 days
