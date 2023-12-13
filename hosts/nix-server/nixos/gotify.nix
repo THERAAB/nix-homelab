97284836569a80a3db19dcf8897c15311741602e
THERAAB <rob.w.lago@gmail.com>
2023-12-13 16:42:25 -0500

    add gotify

--- hosts/nix-server/nixos/gotify.nix
+++ hosts/nix-server/nixos/gotify.nix
@@ -1,15 +1,38 @@
 {...}: let
   port = 8238;
+  app-name = "gotify";
   network = import ../../../share/network.properties.nix;
 in {
-  services.caddy.virtualHosts."gotify.${network.domain}" = {
+  services.yamlConfigMaker.gatus.settings.endpoints = [
+    {
+      name = "Gotify";
+      url = "https://${app-name}.${network.domain}/";
+      conditions = [
+        "[STATUS] == 200"
+      ];
+      alerts = [
+        {
+          type = "custom";
+        }
+      ];
+    }
+  ];
+  services.olivetin.settings.actions = [
+    {
+      title = "Restart Gotify";
+      icon = ''<img src = "customIcons/${app-name}.png" width = "48px"/>'';
+      shell = "sudo /var/lib/olivetin/scripts/commands.sh -s ${app-name}";
+      timeout = 20;
+    }
+  ];
+  services.caddy.virtualHosts."${app-name}.${network.domain}" = {
     useACMEHost = "${network.domain}";
     extraConfig = ''
       encode zstd gzip
       reverse_proxy 127.0.0.1:${toString port}
     '';
   };
-  services.gotify = {
+  services.${app-name} = {
     enable = true;
     port = port;
   };
