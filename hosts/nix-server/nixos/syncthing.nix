d64cde5e6664c2b92ce4fabd1f21e84149217e2d
THERAAB <rob.w.lago@gmail.com>
2023-12-19 06:51:33 -0500

    add syncthing camera

--- hosts/nix-server/nixos/syncthing.nix
+++ hosts/nix-server/nixos/syncthing.nix
@@ -35,9 +35,9 @@ in {
   };
   systemd.tmpfiles.rules = [
     "d    ${local-dir}        -       -             -               -   - "
-    "Z    ${local-dir}        770     syncthing     ${app-name}     -   - "
     "d    ${local-dir}/share  -       -             -               -   - "
-    "Z    ${local-dir}/share  770     syncthing     ${app-name}     -   - "
+    "d    ${local-dir}/Camera  -       -             -               -   - "
+    "Z    ${local-dir}        770     syncthing     ${app-name}     -   - "
   ];
   services.syncthing = {
     enable = true;
