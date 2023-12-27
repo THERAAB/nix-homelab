54ca36bfaa13abd4cc6b65dc7a4187824b9f8af7
THERAAB <rob.w.lago@gmail.com>
2023-12-26 21:09:19 -0500

    add nix-nas

--- share/network.properties.nix
+++ share/network.properties.nix
@@ -11,6 +11,10 @@
     tailscale.ip = "100.126.231.47";
     local.ip = "192.168.1.5";
   };
+  nix-nas = {
+    tailscale.ip = "100.122.29.88";
+    local.ip = "192.168.2.2";
+  };
   pfSense.local.ip = "192.168.1.1";
   unifi-u6-plus.local.ip = "192.168.1.2";
   unifi-usw-lite-8.local.ip = "192.168.1.3";
