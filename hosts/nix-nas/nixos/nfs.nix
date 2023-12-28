b4bb66d30802571b851bf6995219836f5d944592
THERAAB <rob.w.lago@gmail.com>
2023-12-28 08:39:03 -0500

    add nfs

--- hosts/nix-nas/nixos/nfs.nix
+++ hosts/nix-nas/nixos/nfs.nix
@@ -1,13 +1,17 @@
 {...}: let
   nfs-dir = "/nfs";
-  media-dir = "/${nfs-dir}/media";
+  media-dir = "${nfs-dir}/media";
   network = import ../../../share/network.properties.nix;
 in {
   systemd.tmpfiles.rules = [
-    "d    ${nfs-dir}        -    -        -        -   - "
-    "Z    ${nfs-dir}        777  nobody   nogroup  -   - "
+    "d    /nix/persist${nfs-dir}  -    -        -        -   - "
+    "Z    ${nfs-dir}              777  nobody   nogroup  -   - "
   ];
-  fileSystems."/${media-dir}" = {
+  fileSystems."${nfs-dir}" = {
+    device = "/nix/persist${nfs-dir}";
+    options = ["bind"];
+  };
+  fileSystems."${media-dir}" = {
     device = "/dev/disk/by-label/media";
     fsType = "btrfs";
     options = ["subvol=media" "compress=zstd" "noatime"];
@@ -16,8 +20,8 @@ in {
   services.nfs.server = {
     enable = true;
     exports = ''
-      /${nfs-dir}    ${network.nix-server.local.ip}(rw,fsid=0,no_subtree_check) ${network.nix-server.tailscale.ip}(rw,fsid=0,no_subtree_check)
-      /${media-dir}  ${network.nix-server.local.ip}(rw,nohide,insecure,no_subtree_check) ${network.nix-server.tailscale.ip}(rw,nohide,insecure,no_subtree_check)
+      ${nfs-dir}    ${network.nix-server.local.ip}(rw,fsid=0,no_subtree_check) ${network.nix-server.tailscale.ip}(rw,fsid=0,no_subtree_check)
+      ${media-dir}  ${network.nix-server.local.ip}(rw,nohide,insecure,no_subtree_check) ${network.nix-server.tailscale.ip}(rw,nohide,insecure,no_subtree_check)
     '';
   };
 }
