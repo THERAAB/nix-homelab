7a615f013b908c7e17d5d8ec5afec010de3262e7
THERAAB <rob.w.lago@gmail.com>
2023-12-29 23:48:34 -0500

    acpi_9 interrupt

--- hosts/nix-nas/nixos/hardware-configuration.nix
+++ hosts/nix-nas/nixos/hardware-configuration.nix
@@ -12,7 +12,7 @@
   boot.initrd.kernelModules = [];
   boot.kernelModules = ["kvm-intel"];
   boot.extraModulePackages = [];
-  boot.kernelParams = ["pci=noacpi"];
+  # boot.kernelParams = ["i915.force_probe=4692" "i915.enable_guc=3"];
 
   fileSystems."/" = {
     device = "none";
