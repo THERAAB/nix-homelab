61e297cc8b95d5a4d766271a47c4c5ac06bcd685
THERAAB <rob.w.lago@gmail.com>
2023-12-30 07:44:17 -0500

    acpi_9

--- hosts/nix-nas/nixos/hardware.nix
+++ hosts/nix-nas/nixos/hardware.nix
@@ -1,3 +1,6 @@
 {...}: {
   networking.hostName = "nix-nas"; # Define your hostname.
+  powerManagement.powerUpCommands = ''
+    echo disable > /sys/firmware/acpi/interrupts/gpe6F
+  ''; # Disable otherwise ACPI IRQ Routing for interrupt 9 consumes 20% CPU 
 }
