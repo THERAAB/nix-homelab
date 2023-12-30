96cedc09594b72510dd7ab957408f08dee555588
THERAAB <rob.w.lago@gmail.com>
2023-12-29 23:19:07 -0500

    acpi_9 interrupt

--- hosts/nix-nas/nixos/hardware-configuration.nix
+++ hosts/nix-nas/nixos/hardware-configuration.nix
@@ -12,7 +12,7 @@
   boot.initrd.kernelModules = [];
   boot.kernelModules = ["kvm-intel"];
   boot.extraModulePackages = [];
-  boot.kernelParams = ["acpi=ht"];
+  boot.kernelParams = ["noapic"];
 
   fileSystems."/" = {
     device = "none";
