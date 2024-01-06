89784c0dadecf15afdad7ca3f06b4d0c634268d0
THERAAB <rob.w.lago@gmail.com>
2024-01-06 10:40:38 -0500

    rename apps

--- hosts/nix-server/nixos/media/radarr.nix
+++ hosts/nix-server/nixos/media/radarr.nix
@@ -11,7 +11,7 @@ in {
   services.yamlConfigMaker.gatus.settings.endpoints = [
     {
       name = "${display-name}";
-      url = "https://${app-name}.${network.domain}/health";
+      url = "https://movies.${network.domain}/health";
       conditions = [
         "[STATUS] == 401"
       ];
@@ -42,7 +42,7 @@ in {
     "d    ${local-config-dir}     -       -             -           -   - "
     "Z    ${local-config-dir}     740     ${app-name}   ${app-name} -   - "
   ];
-  services.caddy.virtualHosts."${app-name}.${network.domain}" = {
+  services.caddy.virtualHosts."movies.${network.domain}" = {
     useACMEHost = "${network.domain}";
     extraConfig = ''
       encode zstd gzip
