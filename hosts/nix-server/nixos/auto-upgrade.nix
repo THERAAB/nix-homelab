765ec366f2e0961e5c7f1ca6b2f01befb7f8b51b
THERAAB <rob.w.lago@gmail.com>
2024-01-15 07:28:23 -0500

    auto-upgrade

--- hosts/nix-server/nixos/auto-upgrade.nix
+++ hosts/nix-server/nixos/auto-upgrade.nix
@@ -1,33 +1,3 @@
-{
-  config,
-  pkgs,
-  ...
-}: {
-  systemd = {
-    services.nix-flake-update = {
-      script = ''
-        sleep 10
-        dir=/nix/persist/nix-homelab
-        su -c "git -C $dir pull" raab
-        nix flake update $dir --commit-lock-file
-        su -c "git -C $dir push" raab
-      '';
-      path = with pkgs; [
-        gitMinimal
-        config.nix.package.out
-        config.programs.ssh.package
-        su
-        coreutils-full
-      ];
-    };
-    timers.nix-flake-update = {
-      wantedBy = ["timers.target"];
-      timerConfig = {
-        OnCalendar = "Sun *-*-* 04:10:00";
-        Persistent = "true";
-        Unit = "nix-flake-update.service";
-      };
-    };
-  };
+{...}: {
   system.autoUpgrade.dates = "Sun *-*-* 04:20:00";
 }
--- share/nixos/auto-upgrade.nix
+++ share/nixos/auto-upgrade.nix
@@ -6,17 +6,43 @@
 }: let
   network = import ../network.properties.nix;
 in {
-  systemd.services = {
-    nixos-upgrade.onFailure = ["nixos-upgrade-on-failure.service"];
-    nixos-upgrade-on-failure.script = ''
-      TOKEN=`cat ${config.sops.secrets.gotify_homelab_token.path}`
-      HOSTNAME=`${pkgs.nettools}/bin/hostname`
+  systemd = {
+    services = {
+      nixos-upgrade.onFailure = ["nixos-upgrade-on-failure.service"];
+      nixos-upgrade-on-failure.script = ''
+        TOKEN=`cat ${config.sops.secrets.gotify_homelab_token.path}`
+        HOSTNAME=`${pkgs.nettools}/bin/hostname`
 
-      ${pkgs.curl}/bin/curl   https://gotify.${network.domain}/message?token=$TOKEN                                   \
-                              -F "title='$HOSTNAME' Upgrade Failed"                                                   \
-                              -F "message=Upgrade failed on '$HOSTNAME', run journalctl -u nixos-upgrade for details" \
-                              -F "priority=5"                                                                         \
-    '';
+        ${pkgs.curl}/bin/curl   https://gotify.${network.domain}/message?token=$TOKEN                                   \
+                                -F "title='$HOSTNAME' Upgrade Failed"                                                   \
+                                -F "message=Upgrade failed on '$HOSTNAME', run journalctl -u nixos-upgrade for details" \
+                                -F "priority=5"                                                                         \
+      '';
+      nix-flake-update = {
+        script = ''
+          sleep 10
+          dir=/nix/persist/nix-homelab
+          su -c "git -C $dir pull" raab
+          nix flake update $dir --commit-lock-file
+          su -c "git -C $dir push" raab
+        '';
+        path = with pkgs; [
+          gitMinimal
+          config.nix.package.out
+          config.programs.ssh.package
+          su
+          coreutils-full
+        ];
+      };
+    };
+    timers.nix-flake-update = {
+      wantedBy = ["timers.target"];
+      timerConfig = {
+        OnCalendar = "Sun *-*-* 04:10:00";
+        Persistent = "true";
+        Unit = "nix-flake-update.service";
+      };
+    };
   };
   system.autoUpgrade = {
     enable = true;
