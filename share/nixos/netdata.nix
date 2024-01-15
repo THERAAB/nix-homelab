2cd7fc7e4b18dbbfaabe55ee3b05e100d571df5f
THERAAB <rob.w.lago@gmail.com>
2024-01-14 19:10:35 -0500

    netdata gotify

--- share/nixos/netdata.nix
+++ share/nixos/netdata.nix
@@ -27,16 +27,16 @@ in {
         "registry to announce" = "https://${app-name}.${network.domain}/";
       };
     };
-    systemd.services.netdata-write-gotify-conf = {
-      script = ''
-        NOTIFY_CONF_DIR=/etc/netdata/conf.d
-        GOTIFY_TOKEN=`cat ${config.sops.secrets.gotify_gatus_token.path}`
-        echo SEND_GOTIFY="YES" > $NOTIFY_CONF_DIR
-        echo GOTIFY_APP_URL="https://gotify.${network.domain}/" >> $NOTIFY_CONF_DIR
-        echo GOTIFY_TOKEN="$GOTIFY_TOKEN" >> $NOTIFY_CONF_DIR
-      '';
-      wantedBy = ["netdata.service"];
-      after = ["netdata.service"];
-    };
+  };
+  systemd.services.netdata-write-gotify-conf = {
+    script = ''
+      NOTIFY_CONF_DIR=/etc/netdata/conf.d
+      GOTIFY_TOKEN=`cat ${config.sops.secrets.gotify_gatus_token.path}`
+      echo SEND_GOTIFY="YES" > $NOTIFY_CONF_DIR
+      echo GOTIFY_APP_URL="https://gotify.${network.domain}/" >> $NOTIFY_CONF_DIR
+      echo GOTIFY_TOKEN="$GOTIFY_TOKEN" >> $NOTIFY_CONF_DIR
+    '';
+    wantedBy = ["netdata.service"];
+    after = ["netdata.service"];
   };
 }
