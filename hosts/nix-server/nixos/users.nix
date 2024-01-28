d614f9e3718a35a64258ce50ed711aeb44412445
THERAAB <rob.w.lago@gmail.com>
2024-01-28 07:08:55 -0500

    update photoprism

--- hosts/nix-server/nixos/users.nix
+++ hosts/nix-server/nixos/users.nix
@@ -1,5 +1,6 @@
 {...}: {
   users.users.raab.extraGroups = ["syncthing"];
+  users.users.root.extraGroups = ["syncthing"];
   # Sudo
   security.sudo.extraConfig = ''
     olivetin ALL=(root) NOPASSWD:/var/lib/olivetin/scripts/commands.sh
