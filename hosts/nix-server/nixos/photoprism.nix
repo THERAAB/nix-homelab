e3357208dc81063564b5d62cb7f419c1e766ed8f
THERAAB <rob.w.lago@gmail.com>
2024-01-28 07:18:18 -0500

    update photoprism

--- hosts/nix-server/nixos/photoprism.nix
+++ hosts/nix-server/nixos/photoprism.nix
@@ -50,7 +50,7 @@ in {
   systemd = {
     services."${app-name}-index-refresh" = {
       script = ''
-        ${pkgs.photoprism}/bin/${app-name} --read-only --originals-path ${originals-dir} --originals-limit -1 --upload-nsfw index --cleanup
+        /var/lib/private/photoprism/photoprism-manage index --cleanup
       '';
       after = ["${app-name}.service"];
       requires = ["${app-name}.service"];
--- hosts/nix-server/nixos/users.nix
+++ hosts/nix-server/nixos/users.nix
@@ -1,6 +1,5 @@
 {...}: {
   users.users.raab.extraGroups = ["syncthing"];
-  users.users.root.extraGroups = ["syncthing" "photoprism"];
   # Sudo
   security.sudo.extraConfig = ''
     olivetin ALL=(root) NOPASSWD:/var/lib/olivetin/scripts/commands.sh
