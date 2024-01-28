46a4587182c8c7f4ab11955b7020b85419d878f2
THERAAB <rob.w.lago@gmail.com>
2024-01-28 14:56:45 -0500

    restic

--- hosts/nix-server/nixos/restic.nix
+++ hosts/nix-server/nixos/restic.nix
@@ -1,2 +1,21 @@
-{...}: {
+{config, ...}: {
+  #TODO: enable in default.nix
+  services.restic.backups.server = {
+    exclude = [
+      "*/.git"
+    ];
+    initialize = true;
+    passwordFile = config.sops.secrets.restic_password.path;
+    paths = [
+      "/nix/persist"
+      "/sync"
+    ];
+    repository = "/backups";
+    pruneOpts = [
+      "--keep-daily 7"
+      "--keep-weekly 5"
+      "--keep-monthly 12"
+      "--keep-yearly 75"
+    ];
+  };
 }
