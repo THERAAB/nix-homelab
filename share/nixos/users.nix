82f2e4c65dfe125d94b5a430244e715d509856a5
THERAAB <rob.w.lago@gmail.com>
2024-01-28 16:45:45 -0500

    restic

--- share/nixos/users.nix
+++ share/nixos/users.nix
@@ -8,7 +8,10 @@
     };
     users = {
       # $ nix-shell --run 'mkpasswd -m SHA-512 -s' -p mkpasswd
-      root.initialHashedPassword = "$6$89oRObq6lBVqE6Pz$lsm8TXKsGsYZ3HsmbshaTRbecNRMtRQmUUWZS2fubs6y8vF9lLp01dbrhgjGvGPco7qLdEN.hRQ0uisscBanM1";
+      root = {
+        extraGroups = ["restic"];
+        initialHashedPassword = "$6$89oRObq6lBVqE6Pz$lsm8TXKsGsYZ3HsmbshaTRbecNRMtRQmUUWZS2fubs6y8vF9lLp01dbrhgjGvGPco7qLdEN.hRQ0uisscBanM1";
+      };
       raab = {
         isNormalUser = true;
         description = "raab";
