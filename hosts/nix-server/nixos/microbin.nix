4f923b6368582df3660a4adf7773d7940230c363
THERAAB <rob.w.lago@gmail.com>
2024-02-02 16:39:32 -0500

    code cleanup

--- hosts/nix-server/nixos/microbin.nix
+++ hosts/nix-server/nixos/microbin.nix
@@ -5,20 +5,20 @@
   network = import ../../../share/network.properties.nix;
 in {
   networking.firewall.allowedTCPPorts = [port];
-  #services.yamlConfigMaker.gatus.settings.endpoints = [
-  #  {
-  #    name = "${display-name}";
-  #    url = "https://${app-name}.${network.domain}";
-  #    conditions = [
-  #      "[STATUS] == 200"
-  #    ];
-  #    alerts = [
-  #      {
-  #        type = "gotify";
-  #      }
-  #    ];
-  #  }
-  #];
+  services.yamlConfigMaker.gatus.settings.endpoints = [
+    {
+      name = "${display-name}";
+      url = "https://${app-name}.${network.domain}";
+      conditions = [
+        "[STATUS] == 200"
+      ];
+      alerts = [
+        {
+          type = "gotify";
+        }
+      ];
+    }
+  ];
   services.olivetin.settings.actions = [
     {
       title = "Restart ${display-name}";
@@ -27,7 +27,7 @@ in {
       timeout = 20;
     }
   ];
-   services.caddy.virtualHosts."${app-name}.${network.domain}" = {
+  services.caddy.virtualHosts."${app-name}.${network.domain}" = {
     useACMEHost = "${network.domain}";
     extraConfig = ''
       encode zstd gzip
