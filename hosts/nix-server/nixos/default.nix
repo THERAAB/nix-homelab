46a14f6ff5ff5dd1517f21e37ace8a2428bfc574
THERAAB <rob.w.lago@gmail.com>
2024-02-02 11:19:15 -0500

    code cleanup

--- hosts/nix-server/nixos/default.nix
+++ hosts/nix-server/nixos/default.nix
@@ -1,16 +1,16 @@
 {...}: {
   imports = [
-    #./home-assistant
-    #./homer
-    #./media
-    #./gatus
-    #./olivetin
-    #./adguard
+    ./home-assistant
+    ./homer
+    ./media
+    ./gatus
+    ./olivetin
+    ./adguard
     ./hardware-configuration.nix
-    #./caddy.nix
-    #./netdata.nix
-    #./users.nix
-    ./hardware.nix
+    ./caddy.nix
+    ./netdata.nix
+    ./users.nix
+    #./hardware.nix
     ./sops.nix
     ./photoprism.nix
     ./unifi.nix
