331beb4ad9d8395da905aca0ccbcf5118ca9b1f7
THERAAB <rob.w.lago@gmail.com>
2024-02-02 16:23:52 -0500

    code cleanup

--- hosts/nix-server/nixos/microbin.nix
+++ hosts/nix-server/nixos/microbin.nix
@@ -19,28 +19,26 @@ in {
       ];
     }
   ];
-  services = {
-    olivetin.settings.actions = [
-      {
-        title = "Restart ${display-name}";
-        icon = ''<img src = "customIcons/${app-name}.png" width = "48px"/>'';
-        shell = "sudo /var/lib/olivetin/scripts/commands.sh -s ${app-name}";
-        timeout = 20;
-      }
-    ];
-    caddy.virtualHosts."${app-name}.${network.domain}" = {
-      useACMEHost = "${network.domain}";
-      extraConfig = ''
-        encode zstd gzip
-        reverse_proxy 127.0.0.1:${toString port}
-      '';
-    };
-    microbin = {
-      enable = true;
-      passwordFile = config.sops.secrets.df_password.path;
-      settings = {
-        MICROBIN_PORT = port;
-      };
+  services.olivetin.settings.actions = [
+    {
+      title = "Restart ${display-name}";
+      icon = ''<img src = "customIcons/${app-name}.png" width = "48px"/>'';
+      shell = "sudo /var/lib/olivetin/scripts/commands.sh -s ${app-name}";
+      timeout = 20;
+    }
+  ];
+  services.caddy.virtualHosts."${app-name}.${network.domain}" = {
+    useACMEHost = "${network.domain}";
+    extraConfig = ''
+      encode zstd gzip
+      reverse_proxy 127.0.0.1:${toString port}
+    '';
+  };
+  services.microbin = {
+    enable = true;
+    passwordFile = config.sops.secrets.df_password.path;
+    settings = {
+      MICROBIN_PORT = port;
     };
   };
 }
