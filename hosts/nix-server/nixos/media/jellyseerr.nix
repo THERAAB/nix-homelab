bb9d7761deec6026352e7943bdae21c2d07004ac
THERAAB <rob.w.lago@gmail.com>
2024-02-02 10:57:50 -0500

    code cleanup

--- hosts/nix-server/nixos/media/jellyseerr.nix
+++ hosts/nix-server/nixos/media/jellyseerr.nix
@@ -8,35 +8,37 @@
   local-config-dir = "/var/lib/${app-name}/";
   network = import ../../../../share/network.properties.nix;
 in {
-  yamlConfigMaker = {
-    gatus.settings.endpoints = [
-      {
-        name = "${display-name}";
-        url = "https://${app-name}.${network.domain}/health";
-        conditions = [
-          "[STATUS] == 200"
-        ];
-        alerts = [
-          {
-            type = "gotify";
-          }
-        ];
-      }
-    ];
-    olivetin.settings.actions = [
-      {
-        title = "Restart ${display-name}";
-        icon = ''<img src = "customIcons/${app-name}.png" width = "48px"/>'';
-        shell = "sudo /var/lib/olivetin/scripts/commands.sh -s podman-${app-name}";
-        timeout = 20;
-      }
-    ];
-    caddy.virtualHosts."${app-name}.${network.domain}" = {
-      useACMEHost = "${network.domain}";
-      extraConfig = ''
-        encode zstd gzip
-        reverse_proxy 127.0.0.1:${toString port}
-      '';
+  services = {
+    yamlConfigMaker = {
+      gatus.settings.endpoints = [
+        {
+          name = "${display-name}";
+          url = "https://${app-name}.${network.domain}/health";
+          conditions = [
+            "[STATUS] == 200"
+          ];
+          alerts = [
+            {
+              type = "gotify";
+            }
+          ];
+        }
+      ];
+      olivetin.settings.actions = [
+        {
+          title = "Restart ${display-name}";
+          icon = ''<img src = "customIcons/${app-name}.png" width = "48px"/>'';
+          shell = "sudo /var/lib/olivetin/scripts/commands.sh -s podman-${app-name}";
+          timeout = 20;
+        }
+      ];
+      caddy.virtualHosts."${app-name}.${network.domain}" = {
+        useACMEHost = "${network.domain}";
+        extraConfig = ''
+          encode zstd gzip
+          reverse_proxy 127.0.0.1:${toString port}
+        '';
+      };
     };
   };
   users = {
