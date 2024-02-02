2039c560148d06a1e2c968e6fa84647450f948c4
THERAAB <rob.w.lago@gmail.com>
2024-02-02 16:54:22 -0500

    code cleanup

--- hosts/nix-nas/nixos/hardware-configuration.nix
+++ hosts/nix-nas/nixos/hardware-configuration.nix
@@ -7,7 +7,6 @@
   imports = [
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
-
   boot = {
     initrd = {
       availableKernelModules = ["xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" "sdhci_pci"];
@@ -16,35 +15,34 @@
     kernelModules = ["kvm-intel"];
     extraModulePackages = [];
   };
-
-  fileSystems."/" = {
-    device = "none";
-    fsType = "tmpfs";
-    options = ["size=8G" "mode=755"];
+  fileSystems = {
+    "/" = {
+      device = "none";
+      fsType = "tmpfs";
+      options = ["size=8G" "mode=755"];
+    };
+    "/home/raab" = {
+      device = "none";
+      fsType = "tmpfs";
+      options = ["size=4G" "mode=777"];
+    };
+    "/nix" = {
+      device = "/dev/disk/by-label/nixos";
+      fsType = "btrfs";
+      options = ["subvol=nix" "compress=zstd" "noatime"];
+    };
+    "/nix/persist" = {
+      device = "/dev/disk/by-label/nixos";
+      fsType = "btrfs";
+      options = ["subvol=persist" "compress=zstd" "noatime"];
+      neededForBoot = true;
+    };
+    "/boot" = {
+      device = "/dev/disk/by-label/BOOT";
+      fsType = "vfat";
+    };
   };
-  fileSystems."/home/raab" = {
-    device = "none";
-    fsType = "tmpfs";
-    options = ["size=4G" "mode=777"];
-  };
-  fileSystems."/nix" = {
-    device = "/dev/disk/by-label/nixos";
-    fsType = "btrfs";
-    options = ["subvol=nix" "compress=zstd" "noatime"];
-  };
-  fileSystems."/nix/persist" = {
-    device = "/dev/disk/by-label/nixos";
-    fsType = "btrfs";
-    options = ["subvol=persist" "compress=zstd" "noatime"];
-    neededForBoot = true;
-  };
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-label/BOOT";
-    fsType = "vfat";
-  };
-
   swapDevices = [];
-
   networking.useDHCP = lib.mkDefault true;
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
