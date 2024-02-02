bf4c125a93e0cd302aa4edc57cb5a4b420d12e26
THERAAB <rob.w.lago@gmail.com>
2024-02-02 16:27:17 -0500

    code cleanup

--- hosts/nix-server/nixos/microbin.nix
+++ hosts/nix-server/nixos/microbin.nix
@@ -5,29 +5,29 @@
   network = import ../../../share/network.properties.nix;
 in {
   networking.firewall.allowedTCPPorts = [port];
-  #services.yamlConfigMaker.gatus.settings.endpoints = [
-  #  {
-  #    name = "${display-name}";
-  #    url = "https://${app-name}.${network.domain}";
-  #    conditions = [
-  #      "[STATUS] == 200"
-  #    ];
-  #    alerts = [
-  #      {
-  #        type = "gotify";
-  #      }
-  #    ];
-  #  }
-  #];
-  #services.olivetin.settings.actions = [
-  #  {
-  #    title = "Restart ${display-name}";
-  #    icon = ''<img src = "customIcons/${app-name}.png" width = "48px"/>'';
-  #    shell = "sudo /var/lib/olivetin/scripts/commands.sh -s ${app-name}";
-  #    timeout = 20;
-  #  }
-  #];
-  #services.caddy.virtualHosts."${app-name}.${network.domain}" = {
+  services.yamlConfigMaker.gatus.settings.endpoints = [
+    {
+      name = "${display-name}";
+      url = "https://${app-name}.${network.domain}";
+      conditions = [
+        "[STATUS] == 200"
+      ];
+      alerts = [
+        {
+          type = "gotify";
+        }
+      ];
+    }
+  ];
+  services.olivetin.settings.actions = [
+    {
+      title = "Restart ${display-name}";
+      icon = ''<img src = "customIcons/${app-name}.png" width = "48px"/>'';
+      shell = "sudo /var/lib/olivetin/scripts/commands.sh -s ${app-name}";
+      timeout = 20;
+    }
+  ];
+  # services.caddy.virtualHosts."${app-name}.${network.domain}" = {
   #  useACMEHost = "${network.domain}";
   #  extraConfig = ''
   #    encode zstd gzip
