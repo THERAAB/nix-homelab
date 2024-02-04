feb517fc6604939429febbd63830be7bfd57b8c3
THERAAB <rob.w.lago@gmail.com>
2024-02-04 07:14:14 -0500

    photoprism

--- hosts/nix-server/nixos/syncthing.nix
+++ hosts/nix-server/nixos/syncthing.nix
@@ -10,9 +10,8 @@ in {
     "d    ${local-dir}/share   -       -             -               -   - "
     "d    ${local-dir}/Camera  -       -             -               -   - "
     "Z    ${local-dir}/share   770     -             ${app-name}     -   - "
-    "Z    ${local-dir}/Camera  -       -             ${app-name}     -   - "
+    "Z    ${local-dir}/Camera  770     -             ${app-name}     -   - "
   ];
-  users.users."${app-name}".extraGroups = ["photoprism"];
   services = {
     yamlConfigMaker.gatus.settings.endpoints = [
       {
