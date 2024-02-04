9f224427492da81d451554717cd07cf3f51a4e45
THERAAB <rob.w.lago@gmail.com>
2024-02-04 06:48:47 -0500

    flatnotes

--- hosts/nix-server/nixos/syncthing.nix
+++ hosts/nix-server/nixos/syncthing.nix
@@ -7,7 +7,7 @@
 in {
   systemd.tmpfiles.rules = [
     "d    ${local-dir}         -       -             -               -   - "
-    # "d    ${local-dir}/share   -       -             -               -   - "
+    "d    ${local-dir}/share   -       -             -               -   - "
     "d    ${local-dir}/Camera  -       -             -               -   - "
     # "Z    ${local-dir}/share   770     ${app-name}   ${app-name}     -   - "
     "Z    ${local-dir}/Camera  -       ${app-name}   ${app-name}     -   - "
