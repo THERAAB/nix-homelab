7cbc0895471514f30bb4376735ccbbbdbd8742d6
THERAAB <rob.w.lago@gmail.com>
2024-02-06 15:31:26 -0500

    microvm

--- hosts/nix-server/nixos/microvm.nix
+++ hosts/nix-server/nixos/microvm.nix
@@ -3,24 +3,9 @@
     "my-microvm"
   ];
   microvm.vms = {
-    my-microvm = {
-      #inherit system;
-      #microvm = {
-      #  mem = 2048;
-      #  vcpu = 1;
-      #};
-      # The package set to use for the microvm. This also determines the microvm's architecture.
-      # Defaults to the host system's package set if not given.
-      # pkgs = import nixpkgs {system = "x86_64-linux";};
-
-      # (Optional) A set of special arguments to be passed to the MicroVM's NixOS modules.
-      #specialArgs = {};
-      # The configuration for the MicroVM.
-      # Multiple definitions will be merged as expected.
-      config = {
-        # It is highly recommended to share the host's nix-store
-        # with the VMs to prevent building huge images.
-        microvm.shares = [
+    my-microvm.config = {
+      microvm = {
+        shares = [
           {
             source = "/nix/store";
             mountPoint = "/nix/.ro-store";
@@ -28,17 +13,12 @@
             proto = "virtiofs";
           }
         ];
-        microvm = {
-          mem = 2048;
-          vcpu = 1;
-        };
-
-        system.stateVersion = "23.11";
-        users.users.root.password = "";
-        networking.hostName = "my-microvm";
-        # Any other configuration for your MicroVM
-        # [...]
+        mem = 2048;
+        vcpu = 1;
       };
+      system.stateVersion = "23.11";
+      users.users.root.password = "";
+      networking.hostName = "my-microvm";
     };
   };
 }
