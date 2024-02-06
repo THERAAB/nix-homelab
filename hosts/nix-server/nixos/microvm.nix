c95c9d6e1863ea5aa08752313ce7b0e6759baaa5
THERAAB <rob.w.lago@gmail.com>
2024-02-06 15:39:59 -0500

    microvm

--- hosts/nix-server/nixos/microvm.nix
+++ hosts/nix-server/nixos/microvm.nix
@@ -18,5 +18,22 @@
       users.users.root.password = "";
       networking.hostName = "my-microvm";
     };
+    vms.my-microvm2.config = {
+      microvm = {
+        shares = [
+          {
+            source = "/nix/store";
+            mountPoint = "/nix/.ro-store";
+            tag = "ro-store";
+            proto = "virtiofs";
+          }
+        ];
+        mem = 2048;
+        vcpu = 1;
+      };
+      system.stateVersion = "23.11";
+      users.users.root.password = "";
+      networking.hostName = "my-microvm2";
+    };
   };
 }
