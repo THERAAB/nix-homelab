cca3bdf228ff377a49610081cdf532e2d792732d
THERAAB <rob.w.lago@gmail.com>
2024-02-10 08:17:50 -0500

    microvm networking

--- hosts/nix-server/nixos/microvm.nix
+++ hosts/nix-server/nixos/microvm.nix
@@ -1,4 +1,45 @@
 {...}: {
+  systemd.network = {
+    enable = true;
+    netdevs.virbr0.netdevConfig = {
+      Kind = "bridge";
+      Name = "virbr0";
+    };
+    networks.virbr0 = {
+      matchConfig.Name = "virbr0";
+      # Hand out IP addresses to MicroVMs.
+      # Use `networkctl status virbr0` to see leases.
+      networkConfig = {
+        DHCPServer = true;
+        IPv6SendRA = true;
+      };
+      addresses = [
+        {
+          addressConfig.Address = "10.0.0.1/24";
+        }
+        {
+          addressConfig.Address = "fd12:3456:789a::1/64";
+        }
+      ];
+      ipv6Prefixes = [
+        {
+          ipv6PrefixConfig.Prefix = "fd12:3456:789a::/64";
+        }
+      ];
+    };
+    networks.microvm-eth0 = {
+      matchConfig.Name = "vm-*";
+      networkConfig.Bridge = "virbr0";
+    };
+  };
+  # Allow DHCP server
+  networking.firewall.allowedUDPPorts = [67];
+  # Allow Internet access
+  networking.nat = {
+    enable = true;
+    enableIPv6 = true;
+    internalInterfaces = ["virbr0"];
+  };
   microvm = {
     autostart = ["my-microvm"];
     vms.my-microvm.config = {
@@ -28,47 +69,6 @@
       services.openssh = {
         enable = true;
         settings.PermitRootLogin = "yes";
-      };
-      systemd.network = {
-        enable = true;
-        netdevs.virbr0.netdevConfig = {
-          Kind = "bridge";
-          Name = "virbr0";
-        };
-        networks.virbr0 = {
-          matchConfig.Name = "virbr0";
-          # Hand out IP addresses to MicroVMs.
-          # Use `networkctl status virbr0` to see leases.
-          networkConfig = {
-            DHCPServer = true;
-            IPv6SendRA = true;
-          };
-          addresses = [
-            {
-              addressConfig.Address = "10.0.0.1/24";
-            }
-            {
-              addressConfig.Address = "fd12:3456:789a::1/64";
-            }
-          ];
-          ipv6Prefixes = [
-            {
-              ipv6PrefixConfig.Prefix = "fd12:3456:789a::/64";
-            }
-          ];
-        };
-        networks.microvm-eth0 = {
-          matchConfig.Name = "vm-*";
-          networkConfig.Bridge = "virbr0";
-        };
-      };
-      # Allow DHCP server
-      networking.firewall.allowedUDPPorts = [67];
-      # Allow Internet access
-      networking.nat = {
-        enable = true;
-        enableIPv6 = true;
-        internalInterfaces = ["virbr0"];
       };
     };
     vms.my-microvm2.config = {
