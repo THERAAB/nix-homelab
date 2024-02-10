66fc2301172d3aa26fcf2c6737ecef3bfb9430a6
THERAAB <rob.w.lago@gmail.com>
2024-02-10 08:28:52 -0500

    microvm networking

--- hosts/nix-server/nixos/microvm.nix
+++ hosts/nix-server/nixos/microvm.nix
@@ -1,36 +1,6 @@
 {...}: {
   systemd.network = {
     enable = true;
-    netdevs.virbr0.netdevConfig = {
-      Kind = "bridge";
-      Name = "virbr0";
-    };
-    networks.virbr0 = {
-      matchConfig.Name = "virbr0";
-      # Hand out IP addresses to MicroVMs.
-      # Use `networkctl status virbr0` to see leases.
-      networkConfig = {
-        DHCPServer = true;
-        IPv6SendRA = true;
-      };
-      addresses = [
-        {
-          addressConfig.Address = "10.0.0.1/24";
-        }
-        {
-          addressConfig.Address = "fd12:3456:789a::1/64";
-        }
-      ];
-      ipv6Prefixes = [
-        {
-          ipv6PrefixConfig.Prefix = "fd12:3456:789a::/64";
-        }
-      ];
-    };
-    networks.microvm-eth0 = {
-      matchConfig.Name = "vm-*";
-      networkConfig.Bridge = "virbr0";
-    };
   };
   # Allow DHCP server
   networking.firewall.allowedUDPPorts = [67];
@@ -38,7 +8,8 @@
   networking.nat = {
     enable = true;
     enableIPv6 = true;
-    internalInterfaces = ["virbr0"];
+    externalInterface = "enp3s0";
+    internalInterfaces = ["microvm"];
   };
   microvm = {
     autostart = ["my-microvm"];
@@ -58,7 +29,7 @@
         interfaces = [
           {
             type = "tap";
-            id = "vm-test1";
+            id = "microvm";
             mac = "02:00:00:00:00:01";
           }
         ];
