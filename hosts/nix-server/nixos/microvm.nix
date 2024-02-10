ac8bc485bf55908114fb66a9d6132ff18f350c11
THERAAB <rob.w.lago@gmail.com>
2024-02-10 07:17:54 -0500

    microvm networking

--- hosts/nix-server/nixos/microvm.nix
+++ hosts/nix-server/nixos/microvm.nix
@@ -2,30 +2,30 @@
   networking.useNetworkd = true;
   systemd.network.enable = true;
 
-  systemd.network.networks."10-lan" = {
-    matchConfig.Name = ["enp3s0" "vm-*"];
-    networkConfig = {
-      Bridge = "br0";
-    };
-  };
+  #systemd.network.networks."10-lan" = {
+  #  matchConfig.Name = ["enp3s0" "vm-*"];
+  #  networkConfig = {
+  #    Bridge = "br0";
+  #  };
+  #};
 
-  systemd.network.netdevs."br0" = {
-    netdevConfig = {
-      Name = "br0";
-      Kind = "bridge";
-    };
-  };
+  #systemd.network.netdevs."br0" = {
+  #  netdevConfig = {
+  #    Name = "br0";
+  #    Kind = "bridge";
+  #  };
+  #};
 
-  systemd.network.networks."10-lan-bridge" = {
-    matchConfig.Name = "br0";
-    networkConfig = {
-      Address = ["192.168.3.2/24"];
-      Gateway = "192.168.3.1";
-      DNS = ["1.1.1.1"];
-      # IPv6AcceptRA = true;
-    };
-    linkConfig.RequiredForOnline = "routable";
-  };
+  #systemd.network.networks."10-lan-bridge" = {
+  #  matchConfig.Name = "br0";
+  #  networkConfig = {
+  #    Address = ["192.168.3.2/24"];
+  #    Gateway = "192.168.3.1";
+  #    DNS = ["1.1.1.1"];
+  #    IPv6AcceptRA = true;
+  #  };
+  #  linkConfig.RequiredForOnline = "routable";
+  #};
   microvm = {
     autostart = ["my-microvm"];
     vms.my-microvm.config = {
@@ -54,16 +54,16 @@
       networking.hostName = "my-microvm";
       systemd.network.enable = true;
 
-      systemd.network.networks."20-lan" = {
-        matchConfig.Type = "ether";
-        networkConfig = {
-          Address = ["192.168.3.3/24"];
-          Gateway = "192.168.3.1";
-          DNS = ["1.1.1.1"];
-          #IPv6AcceptRA = true;
-          DHCP = "no";
-        };
-      };
+      #systemd.network.networks."20-lan" = {
+      #  matchConfig.Type = "ether";
+      #  networkConfig = {
+      #    Address = ["192.168.3.3/24"];
+      #    Gateway = "192.168.3.1";
+      #    DNS = ["1.1.1.1"];
+      #    IPv6AcceptRA = true;
+      #    DHCP = "no";
+      #  };
+      #};
     };
     vms.my-microvm2.config = {
       microvm = {
