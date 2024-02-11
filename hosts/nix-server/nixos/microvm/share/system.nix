e995c483b098d2eb92b80b5938f8183fd9db9ee7
THERAAB <rob.w.lago@gmail.com>
2024-02-11 08:10:25 -0500

    add micro2

--- hosts/nix-server/nixos/microvm/share/system.nix
+++ hosts/nix-server/nixos/microvm/share/system.nix
@@ -15,10 +15,24 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   system.stateVersion = "23.11";
-  users.users.root.password = "";
+  users = {
+    mutableUsers = false;
+    users = {
+      # $ nix-shell --run 'mkpasswd -m SHA-512 -s' -p mkpasswd
+      root = {
+        initialHashedPassword = "$6$89oRObq6lBVqE6Pz$lsm8TXKsGsYZ3HsmbshaTRbecNRMtRQmUUWZS2fubs6y8vF9lLp01dbrhgjGvGPco7qLdEN.hRQ0uisscBanM1";
+      };
+      raab = {
+        isNormalUser = true;
+        description = "raab";
+        extraGroups = ["networkmanager" "wheel"];
+        initialHashedPassword = "$6$89oRObq6lBVqE6Pz$lsm8TXKsGsYZ3HsmbshaTRbecNRMtRQmUUWZS2fubs6y8vF9lLp01dbrhgjGvGPco7qLdEN.hRQ0uisscBanM1";
+        uid = 1000;
+      };
+    };
+  };
   services.openssh = {
     enable = true;
-    settings.PermitRootLogin = "yes";
   };
   networking.nameservers = ["1.1.1.1"];
 }
