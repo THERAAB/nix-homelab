8ce79f792244f9ba7d84ec0e0845bd0cb37d87e8
THERAAB <rob.w.lago@gmail.com>
2024-02-13 06:16:16 -0500

    add everything

--- hosts/nix-server/nixos/microvm/micro1/adguard/default.nix
+++ hosts/nix-server/nixos/microvm/micro1/adguard/default.nix
@@ -6,36 +6,36 @@
   network = import ../../../../../../share/network.properties.nix;
 in {
   services = {
-    yamlConfigMaker.gatus.settings.endpoints = [
-      {
-        name = "${display-name}";
-        url = "https://${app-name}.${network.domain}/";
-        conditions = [
-          "[STATUS] == 200"
-          ''[BODY] == pat(*<title>Login</title>*)''
-        ];
-        alerts = [
-          {
-            type = "gotify";
-          }
-        ];
-      }
-    ];
-    olivetin.settings.actions = [
-      {
-        title = "Restart ${display-name}";
-        icon = ''<img src = "customIcons/${app-name}.png" width = "48px"/>'';
-        shell = "sudo /var/lib/olivetin/scripts/commands.sh -s adguardhome";
-        timeout = 20;
-      }
-    ];
-    caddy.virtualHosts."${app-name}.${network.domain}" = {
-      useACMEHost = "${network.domain}";
-      extraConfig = ''
-        encode zstd gzip
-        reverse_proxy 127.0.0.1:${toString port}
-      '';
-    };
+    #yamlConfigMaker.gatus.settings.endpoints = [
+    #  {
+    #    name = "${display-name}";
+    #    url = "https://${app-name}.${network.domain}/";
+    #    conditions = [
+    #      "[STATUS] == 200"
+    #      ''[BODY] == pat(*<title>Login</title>*)''
+    #    ];
+    #    alerts = [
+    #      {
+    #        type = "gotify";
+    #      }
+    #    ];
+    #  }
+    #];
+    #olivetin.settings.actions = [
+    #  {
+    #    title = "Restart ${display-name}";
+    #    icon = ''<img src = "customIcons/${app-name}.png" width = "48px"/>'';
+    #    shell = "sudo /var/lib/olivetin/scripts/commands.sh -s adguardhome";
+    #    timeout = 20;
+    #  }
+    #];
+    #caddy.virtualHosts."${app-name}.${network.domain}" = {
+    #  useACMEHost = "${network.domain}";
+    #  extraConfig = ''
+    #    encode zstd gzip
+    #    reverse_proxy 127.0.0.1:${toString port}
+    #  '';
+    #};
     adguardhome = {
       mutableSettings = false;
       enable = true;
