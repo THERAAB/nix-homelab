5dcba7a3a21aecd6cf39d2b24a20bcd8d1a4f06e
THERAAB <rob.w.lago@gmail.com>
2024-02-13 16:34:46 -0500

    add everything

--- hosts/nix-server/nixos/microvm/micro1/photoprism.nix
+++ hosts/nix-server/nixos/microvm/micro1/photoprism.nix
@@ -42,13 +42,13 @@ in {
     };
     ${app-name} = {
       enable = true;
-      #  settings = {
-      #    PHOTOPRISM_ADMIN_USER = "raab";
-      #    PHOTOPRISM_UPLOAD_NSFW = "true";
-      #    PHOTOPRISM_ORIGINALS_LIMIT = "-1";
-      #    PHOTOPRISM_READONLY = "true";
-      #  };
-      #  address = "0.0.0.0";
+      settings = {
+        PHOTOPRISM_ADMIN_USER = "raab";
+        PHOTOPRISM_UPLOAD_NSFW = "true";
+        PHOTOPRISM_ORIGINALS_LIMIT = "-1";
+        PHOTOPRISM_READONLY = "true";
+      };
+      address = "0.0.0.0";
       originalsPath = "${originals-dir}";
       # passwordFile = config.sops.secrets.df_password.path;
     };
@@ -64,31 +64,31 @@ in {
       isSystemUser = true;
     };
   };
-  #systemd = {
-  #  services."${app-name}-index-refresh" = {
-  #    script = ''
-  #      export PHOTOPRISM_ADMIN_USER='raab'
-  #      export PHOTOPRISM_HTTP_HOST='0.0.0.0'
-  #      export PHOTOPRISM_HTTP_PORT='2342'
-  #      export PHOTOPRISM_IMPORT_PATH='import'
-  #      export PHOTOPRISM_ORIGINALS_LIMIT='-1'
-  #      export PHOTOPRISM_ORIGINALS_PATH='/var/lib/private/photoprism/originals'
-  #      export PHOTOPRISM_READONLY='true'
-  #      export PHOTOPRISM_STORAGE_PATH='/var/lib/photoprism'
-  #      export PHOTOPRISM_UPLOAD_NSFW='true'
-  #      exec ${pkgs.photoprism}/bin/photoprism index --cleanup
-  #    '';
-  #    after = ["${app-name}.service"];
-  #    requires = ["${app-name}.service"];
-  #  };
-  #  timers."${app-name}-index-refresh" = {
-  #    wantedBy = ["timers.target"];
-  #    timerConfig = {
-  #      OnCalendar = "daily";
-  #      Persistent = "true";
-  #      Unit = "${app-name}-index-refresh.service";
-  #    };
-  #  };
-  #};
+  systemd = {
+    services."${app-name}-index-refresh" = {
+      script = ''
+        export PHOTOPRISM_ADMIN_USER='raab'
+        export PHOTOPRISM_HTTP_HOST='0.0.0.0'
+        export PHOTOPRISM_HTTP_PORT='2342'
+        export PHOTOPRISM_IMPORT_PATH='import'
+        export PHOTOPRISM_ORIGINALS_LIMIT='-1'
+        export PHOTOPRISM_ORIGINALS_PATH='/var/lib/private/photoprism/originals'
+        export PHOTOPRISM_READONLY='true'
+        export PHOTOPRISM_STORAGE_PATH='/var/lib/photoprism'
+        export PHOTOPRISM_UPLOAD_NSFW='true'
+        exec ${pkgs.photoprism}/bin/photoprism index --cleanup
+      '';
+      after = ["${app-name}.service"];
+      requires = ["${app-name}.service"];
+    };
+    timers."${app-name}-index-refresh" = {
+      wantedBy = ["timers.target"];
+      timerConfig = {
+        OnCalendar = "daily";
+        Persistent = "true";
+        Unit = "${app-name}-index-refresh.service";
+      };
+    };
+  };
   networking.firewall.allowedTCPPorts = [port];
 }
