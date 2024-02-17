3cb6b8c6873772fe41dae21df796c49130eaeaf5
THERAAB <rob.w.lago@gmail.com>
2024-02-17 13:12:49 -0500

    podman

--- hosts/nix-server/nixos/sops.nix
+++ hosts/nix-server/nixos/sops.nix
@@ -1,17 +1,17 @@
 {...}: {
   sops.secrets = {
     home_assistant = {
-      owner = "hass";
-      path = "/var/lib/hass/secrets.yaml";
+      #owner = "hass";
+      #path = "/var/lib/hass/secrets.yaml";
     };
     wireguard_mullvad = {
-      owner = "vuetorrent";
+      #owner = "vuetorrent";
     };
     df_password = {};
     cloudflare_dns_secret = {};
     harmonia_secret = {};
     microbin_secret = {};
-    mongo_init.owner = "unifi";
-    mongo_secret.owner = "unifi";
+    mongo_init = {}; #.owner = "unifi";
+    mongo_secret = {}; #.owner = "unifi";
   };
 }
