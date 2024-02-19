17d45c159387e1ba75d1946953efed504045ba4e
THERAAB <rob.w.lago@gmail.com>
2024-02-19 15:57:11 -0500

    fix jellyfin

--- hosts/nix-server/nixos/microvm/share/system.nix
+++ hosts/nix-server/nixos/microvm/share/system.nix
@@ -55,7 +55,20 @@
       neovim
       pciutils
       usbutils
+      linux-firmware
     ];
   };
   networking.firewall.enable = true;
+  nixpkgs.config.packageOverrides = pkgs: {
+    vaapiIntel = pkgs.vaapiIntel.override {enableHybridCodec = true;};
+  };
+  hardware.opengl = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-media-driver # LIBVA_DRIVER_NAME=iHD
+      vaapiIntel # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+      vaapiVdpau
+      libvdpau-va-gl
+    ];
+  };
 }
