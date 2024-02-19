c75d942df39e18ce90bf951cd3b232dbb2918cc3
THERAAB <rob.w.lago@gmail.com>
2024-02-19 07:11:20 -0500

    nfs

---
+++ hosts/nix-server/nixos/microvm/micro1/nfs.nix
@@ -0,0 +1,11 @@
+{...}: let
+  network = import ../../../share/network.properties.nix;
+in {
+  fileSystems = {
+    "/media" = {
+      device = "${network.nix-nas.local.ip}:/nfs/media";
+      fsType = "nfs";
+      options = ["x-systemd.automount" "noauto" "x-systemd.idle-timeout=1200"];
+    };
+  };
+}
