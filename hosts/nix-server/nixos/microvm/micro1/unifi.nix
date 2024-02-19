8026eb97238371536df0a06c92e8e95246e1220c
THERAAB <rob.w.lago@gmail.com>
2024-02-19 06:55:29 -0500

    unifi

--- hosts/nix-server/nixos/microvm/micro1/unifi.nix
+++ hosts/nix-server/nixos/microvm/micro1/unifi.nix
@@ -74,7 +74,7 @@ in {
         PGID = "${toString gid}";
         UMASK = "022";
         TZ = "America/New_York";
-        MONGO_HOST = "localhost";
+        MONGO_HOST = "127.0.0.1";
         MONGO_PORT = "27017";
       };
       environmentFiles = [
