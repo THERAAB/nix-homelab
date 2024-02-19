a47be8cbd74b70aa8e721c91cbc9b985b104c82b
THERAAB <rob.w.lago@gmail.com>
2024-02-19 16:08:28 -0500

    fix jellyfin

--- hosts/nix-server/nixos/microvm/micro1/system.nix
+++ hosts/nix-server/nixos/microvm/micro1/system.nix
@@ -1,6 +1,6 @@
 {...}: {
   microvm = {
-    kernelParams = ["i915.force_probe=4692" "i915.enable_guc=3"];
+    kernelParams = ["i915.force_probe=4692"];
     interfaces = [
       {
         type = "macvtap";
