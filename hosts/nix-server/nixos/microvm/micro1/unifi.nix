c93c3db84bb5fd488e417786c9928a40775d51f5
THERAAB <rob.w.lago@gmail.com>
2024-02-19 07:04:16 -0500

    unifi

--- hosts/nix-server/nixos/microvm/micro1/unifi.nix
+++ hosts/nix-server/nixos/microvm/micro1/unifi.nix
@@ -74,7 +74,7 @@ in {
         PGID = "${toString gid}";
         UMASK = "022";
         TZ = "America/New_York";
-        MONGO_HOST = "192.168.3.3";
+        MONGO_HOST = "${network.micro1.local.ip}";
         MONGO_PORT = "27017";
       };
       environmentFiles = [
