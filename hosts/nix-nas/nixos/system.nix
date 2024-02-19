95cd61188dde66d1324fd941dd408d3999df77af
THERAAB <rob.w.lago@gmail.com>
2024-02-19 07:19:23 -0500

    nfs

--- hosts/nix-nas/nixos/system.nix
+++ hosts/nix-nas/nixos/system.nix
@@ -1,8 +1,8 @@
 {...}: let
   network = import ../../../share/network.properties.nix;
 in {
-  #nix.settings = {
-  #  substituters = ["https://cache.${network.domain}"];
-  #  trusted-public-keys = ["cache.${network.domain}:IqbrtbXMzwCjSVZ/sWowaPXtjS+CtpCpStmabZI2TSo="];
-  #};
+  nix.settings = {
+    substituters = ["https://cache.${network.domain}"];
+    trusted-public-keys = ["cache.${network.domain}:IqbrtbXMzwCjSVZ/sWowaPXtjS+CtpCpStmabZI2TSo="];
+  };
 }
