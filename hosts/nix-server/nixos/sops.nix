1c848160a31aca21a3ba3cf73ba696e511727928
THERAAB <rob.w.lago@gmail.com>
2024-02-20 07:40:59 -0500

    ha & unifi

--- hosts/nix-server/nixos/sops.nix
+++ hosts/nix-server/nixos/sops.nix
@@ -1,18 +1,21 @@
 {...}: {
   sops.secrets = {
-    home_assistant.owner = "hass";
+    home_assistant = {
+      owner = "hass";
+      path = "/var/lib/hass/secrets.yaml";
+    };
     wireguard_mullvad = {};
     df_password = {};
     cloudflare_dns_secret = {};
     harmonia_secret = {};
     microbin_secret = {};
     mongo_init = {
-      #owner = "unifi";
-      #path = "/var/lib/unifi/mongo_init";
+      owner = "unifi";
+      path = "/var/lib/unifi/mongo_init";
     };
     mongo_secret = {
-      #owner = "unifi";
-      #path = "/var/lib/unifi/mongo_secret";
+      owner = "unifi";
+      path = "/var/lib/unifi/mongo_secret";
     };
   };
 }
