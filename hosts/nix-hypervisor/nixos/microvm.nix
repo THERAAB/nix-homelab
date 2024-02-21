9a9b6036148f8c1fe25b0f627686cec06eea1489
THERAAB <rob.w.lago@gmail.com>
2024-02-21 12:57:05 -0500

    micro-server

--- hosts/nix-hypervisor/nixos/microvm.nix
+++ hosts/nix-hypervisor/nixos/microvm.nix
@@ -1,15 +1,15 @@
 {self, ...}: {
   microvm = {
-    autostart = ["micro-media" "micro-server"];
+    autostart = ["micro-media"];
     vms = {
       micro-media = {
         flake = self;
         updateFlake = "git+file:///nix/persist/nix-homelab";
       };
-      micro-server = {
-        flake = self;
-        updateFlake = "git+file:///nix/persist/nix-homelab";
-      };
+      #micro-server = {
+      #  flake = self;
+      #  updateFlake = "git+file:///nix/persist/nix-homelab";
+      #};
     };
   };
 }
