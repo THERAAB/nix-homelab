b8e89b099b6b9d5815d8f9eba649e797a2658bbc
THERAAB <rob.w.lago@gmail.com>
2024-02-21 07:08:33 -0500

    microvm structure

--- hosts/micro1/default.nix
+++ hosts/micro1/default.nix
@@ -1,10 +1,10 @@
 {...}: {
   imports = [
-    #./homer
-    #./media
-    #./gatus
-    #./adguard
-    #./caddy.nix
+    ./homer
+    ./media
+    ./gatus
+    ./adguard
+    ./caddy.nix
     #./users.nix
     #./photoprism.nix
     #./unifi.nix
