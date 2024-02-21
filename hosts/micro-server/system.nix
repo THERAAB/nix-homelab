250b25d59b362202a11102c90ceca00f3f409a7e
THERAAB <rob.w.lago@gmail.com>
2024-02-21 13:08:19 -0500

    micro-server

--- hosts/micro-server/system.nix
+++ hosts/micro-server/system.nix
@@ -2,7 +2,6 @@
   microvm = {
     mem = 8192;
     vcpu = 1;
-    kernelParams = ["i915.force_probe=4692"];
     interfaces = [
       {
         type = "macvtap";
@@ -14,43 +13,43 @@
         mac = "02:00:00:00:00:02";
       }
     ];
-    #shares = [
-    #  {
-    #    proto = "virtiofs";
-    #    source = "/run/secrets";
-    #    mountPoint = "/run/secrets";
-    #    tag = "secrets";
-    #  }
-    #  {
-    #    proto = "virtiofs";
-    #    source = "/var/lib/microvms/micro-server/storage/etc/ssh";
-    #    mountPoint = "/etc/ssh";
-    #    tag = "ssh";
-    #  }
-    #  {
-    #    proto = "virtiofs";
-    #    source = "/var/lib/microvms/micro-server/storage/var/lib";
-    #    mountPoint = "/var/lib";
-    #    tag = "var-lib";
-    #  }
-    #];
+    shares = [
+      {
+        proto = "virtiofs";
+        source = "/run/secrets";
+        mountPoint = "/run/secrets";
+        tag = "secrets";
+      }
+      {
+        proto = "virtiofs";
+        source = "/var/lib/microvms/micro-server/storage/etc/ssh";
+        mountPoint = "/etc/ssh";
+        tag = "ssh";
+      }
+      {
+        proto = "virtiofs";
+        source = "/var/lib/microvms/micro-server/storage/var/lib";
+        mountPoint = "/var/lib";
+        tag = "var-lib";
+      }
+    ];
   };
-  #fileSystems = {
-  #  "/etc/ssh".neededForBoot = true;
-  #  "/var/lib".neededForBoot = true;
-  #};
+  fileSystems = {
+    "/etc/ssh".neededForBoot = true;
+    "/var/lib".neededForBoot = true;
+  };
   networking = {
     hostName = "micro-server";
     firewall = {
-      #trustedInterfaces = ["tailscale0"];
+      trustedInterfaces = ["tailscale0"];
       allowedTCPPorts = [80 443];
       allowedUDPPorts = [53];
     };
     networkmanager.enable = true;
   };
-  #environment.noXlibs = false;
-  #services.tailscale = {
-  #  enable = true;
-  #  extraUpFlags = ["--ssh"];
-  #};
+  environment.noXlibs = false;
+  services.tailscale = {
+    enable = true;
+    extraUpFlags = ["--ssh"];
+  };
 }
