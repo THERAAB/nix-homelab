3df2f07e75a99e0edd18c31403663e00dd81657a
THERAAB <rob.w.lago@gmail.com>
2024-02-21 13:02:26 -0500

    micro-server

--- hosts/nix-hypervisor/nixos/microvm.nix
+++ hosts/nix-hypervisor/nixos/microvm.nix
@@ -6,10 +6,10 @@
         flake = self;
         updateFlake = "git+file:///nix/persist/nix-homelab";
       };
-      #micro-server = {
-      #  flake = self;
-      #  updateFlake = "git+file:///nix/persist/nix-homelab";
-      #};
+      micro-server = {
+        flake = self;
+        updateFlake = "git+file:///nix/persist/nix-homelab";
+      };
     };
   };
 }
