f8b17f4bbfd7626afb9dda738b137ca2c72676a4
THERAAB <rob.w.lago@gmail.com>
2024-02-23 11:35:26 -0500

    refactoring

--- share/all/users.nix
+++ share/all/users.nix
@@ -13,7 +13,7 @@
       raab = {
         isNormalUser = true;
         description = "raab";
-        extraGroups = ["networkmanager" "wheel" "keys"]; #TODO: why?
+        extraGroups = ["networkmanager" "wheel" "keys"]; 
         initialHashedPassword = "$6$89oRObq6lBVqE6Pz$lsm8TXKsGsYZ3HsmbshaTRbecNRMtRQmUUWZS2fubs6y8vF9lLp01dbrhgjGvGPco7qLdEN.hRQ0uisscBanM1";
         uid = 1000;
       };
