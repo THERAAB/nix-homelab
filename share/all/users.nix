8bbbbad21ab70f35de2bc941aa2119775945ee14
THERAAB <rob.w.lago@gmail.com>
2024-02-24 08:02:21 -0500

    nfs

--- share/all/users.nix
+++ share/all/users.nix
@@ -9,25 +9,25 @@ in {
       media.gid = users.media.gid;
       restic.gid = users.restic.gid;
     };
-  };
-  users = {
-    # $ nix-shell --run 'mkpasswd -m SHA-512 -s' -p mkpasswd
-    root = {
-      extraGroups = ["restic"];
-      initialHashedPassword = "$6$89oRObq6lBVqE6Pz$lsm8TXKsGsYZ3HsmbshaTRbecNRMtRQmUUWZS2fubs6y8vF9lLp01dbrhgjGvGPco7qLdEN.hRQ0uisscBanM1";
-    };
-    raab = {
-      isNormalUser = true;
-      description = "raab";
-      extraGroups = ["networkmanager" "wheel" "keys"];
-      initialHashedPassword = "$6$89oRObq6lBVqE6Pz$lsm8TXKsGsYZ3HsmbshaTRbecNRMtRQmUUWZS2fubs6y8vF9lLp01dbrhgjGvGPco7qLdEN.hRQ0uisscBanM1";
-      uid = 1000;
-    };
-    nfsnobody = {
-      uid = users.nfsnobody.uid;
-      group = "nfsnobody";
-      extraGroups = ["media" "restic"];
-      isSystemUser = true;
+    users = {
+      # $ nix-shell --run 'mkpasswd -m SHA-512 -s' -p mkpasswd
+      root = {
+        extraGroups = ["restic"];
+        initialHashedPassword = "$6$89oRObq6lBVqE6Pz$lsm8TXKsGsYZ3HsmbshaTRbecNRMtRQmUUWZS2fubs6y8vF9lLp01dbrhgjGvGPco7qLdEN.hRQ0uisscBanM1";
+      };
+      raab = {
+        isNormalUser = true;
+        description = "raab";
+        extraGroups = ["networkmanager" "wheel" "keys"];
+        initialHashedPassword = "$6$89oRObq6lBVqE6Pz$lsm8TXKsGsYZ3HsmbshaTRbecNRMtRQmUUWZS2fubs6y8vF9lLp01dbrhgjGvGPco7qLdEN.hRQ0uisscBanM1";
+        uid = 1000;
+      };
+      nfsnobody = {
+        uid = users.nfsnobody.uid;
+        group = "nfsnobody";
+        extraGroups = ["media" "restic"];
+        isSystemUser = true;
+      };
     };
   };
 }
