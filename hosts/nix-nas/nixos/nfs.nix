7e994fac214020f131be2d3ddedcf5090a48ef6f
THERAAB <rob.w.lago@gmail.com>
2024-02-24 08:15:25 -0500

    nfs

--- hosts/nix-nas/nixos/nfs.nix
+++ hosts/nix-nas/nixos/nfs.nix
@@ -8,7 +8,7 @@ in {
   systemd.tmpfiles.rules = [
     "d    ${nfs-dir}      755  -  -       -   - "
     "Z    ${media-dir}    775  -  media   -   - "
-    "Z    ${backups-dir}  755  -  restic  -   - "
+    "Z    ${backups-dir}  775  -  restic  -   - "
   ];
   fileSystems = {
     "${media-dir}" = {
@@ -28,7 +28,7 @@ in {
     exports = ''
       ${nfs-dir}    ${network.nix-hypervisor.local.ip}(rw,fsid=0,no_subtree_check) ${network.nix-hypervisor.tailscale.ip}(rw,fsid=0,no_subtree_check)
       ${media-dir}  ${network.micro-media.local.ip}(rw,nohide,insecure,no_subtree_check,anonuid=${toString users.nfsnobody.uid}) ${network.micro-media.tailscale.ip}(rw,nohide,insecure,no_subtree_check,anonuid=${toString users.nfsnobody.uid})
-      ${backups-dir}  ${network.nix-hypervisor.local.ip}(rw,nohide,insecure,no_subtree_check,anonuid=${toString users.nfsnobody.uid},no_root_squash) ${network.nix-hypervisor.tailscale.ip}(rw,nohide,insecure,no_subtree_check,anonuid=${toString users.nfsnobody.uid},no_root_squash)
-    ''; #TODO: remove no_root_squash
+      ${backups-dir}  ${network.nix-hypervisor.local.ip}(rw,nohide,insecure,no_subtree_check,anonuid=${toString users.nfsnobody.uid}) ${network.nix-hypervisor.tailscale.ip}(rw,nohide,insecure,no_subtree_check,anonuid=${toString users.nfsnobody.uid})
+    '';
   };
 }
