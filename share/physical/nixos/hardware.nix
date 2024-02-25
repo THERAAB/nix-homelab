0993aa75e991390d6271d925b18d77794516ad45
THERAAB <rob.w.lago@gmail.com>
2024-02-25 11:45:52 -0500

    nix-nas reboot

--- share/physical/nixos/hardware.nix
+++ share/physical/nixos/hardware.nix
@@ -1,6 +1,6 @@
 {pkgs, ...}: {
   boot = {
-    kernelPackages = pkgs.linuxPackages_latest;
+    kernelPackages = pkgs.linuxPackages_6_6;
     loader = {
       systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
