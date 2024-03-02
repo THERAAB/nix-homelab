2c9dd39e4ee44f89e096d8a745f83dbd832190ce
THERAAB <rob.w.lago@gmail.com>
2024-03-02 08:04:30 -0500

    micro-unifi

--- share/microvm/configuration.nix
+++ share/microvm/configuration.nix
@@ -6,14 +6,6 @@
   ...
 }: {
   nix = {
-    # This will add each flake input as a registry
-    # To make nix3 commands consistent with your flake
-    registry = lib.mapAttrs (_: value: {flake = value;}) inputs;
-
-    # This will additionally add your inputs to the system's legacy channels
-    # Making legacy nix commands consistent as well, awesome!
-    nixPath = lib.mapAttrsToList (key: value: "${key}=${value.to.path}") config.nix.registry;
-
     # Flake setup
     package = pkgs.nixVersions.stable;
 
