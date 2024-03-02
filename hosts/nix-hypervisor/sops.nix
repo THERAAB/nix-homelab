32f99fefe56419549e085fcf055d2438d5717aac
THERAAB <rob.w.lago@gmail.com>
2024-03-02 07:01:16 -0500

    secrets refactoring

--- hosts/nix-hypervisor/sops.nix
+++ hosts/nix-hypervisor/sops.nix
@@ -1,6 +1,6 @@
 {...}: {
   systemd.tmpfiles.rules = [
-    "L  /run/secrets/cloudflare_dns_secret  -   -   -   -   /var/lib/microvms/micro-media/storage/run/secrets/cloudflare_dns_secret   "
+    "C  /run/secrets/cloudflare_dns_secret  -   -   -   -   /var/lib/microvms/micro-media/storage/run/secrets/cloudflare_dns_secret   "
   ];
   sops.secrets = {
     home_assistant = {
