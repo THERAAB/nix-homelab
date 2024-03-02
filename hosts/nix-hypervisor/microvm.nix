5494749f49e938ddbb0474c97c1fc0f0aec6f642
THERAAB <rob.w.lago@gmail.com>
2024-03-02 07:53:52 -0500

    micro-unifi

--- hosts/nix-hypervisor/microvm.nix
+++ hosts/nix-hypervisor/microvm.nix
@@ -15,7 +15,8 @@
         updateFlake = "git+file:///nix/persist/nix-homelab";
       };
       micro-unifi = {
-        specialArgs = outputs;
+        #specialArgs = outputs;
+        inherit (self) outputs;
         config = {
           imports = [
             ../../share/lib/modules/nixos/yamlConfigMaker
