d4aedb5ba328359f6ec72dff603f90b937aa5a9e
THERAAB <rob.w.lago@gmail.com>
2024-03-03 17:57:15 -0500

    gotify

--- hosts/micro-monitor/gotify.nix
+++ hosts/micro-monitor/gotify.nix
@@ -6,7 +6,7 @@ in {
     enable = true;
     port = port;
   };
-  environment = {
+  environment.variables = {
     GOTIFY_SERVER_SSL_ENABLED = false;
     GOTIFY_SERVER_SSL_REDIRECTTOHTTPS = false;
     GOTIFY_SERVER_SSL_LETSENCRYPT_ENABLED = false;
