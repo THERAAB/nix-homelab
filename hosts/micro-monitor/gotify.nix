2c48de333a58106ea3bffb90d57e04f7a7312097
THERAAB <rob.w.lago@gmail.com>
2024-03-03 18:05:09 -0500

    gotify

--- hosts/micro-monitor/gotify.nix
+++ hosts/micro-monitor/gotify.nix
@@ -7,9 +7,4 @@ in {
     enable = true;
     port = port;
   };
-  systemd.services.gotify-server.environment = {
-    GOTIFY_SERVER_SSL_ENABLED = "false";
-    GOTIFY_SERVER_SSL_REDIRECTTOHTTPS = "false";
-    GOTIFY_SERVER_SSL_LETSENCRYPT_ENABLED = "false";
-  };
 }
