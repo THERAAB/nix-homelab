fb214f13936dfe717bfdb81e455ada778fb60057
THERAAB <rob.w.lago@gmail.com>
2024-03-05 17:42:23 -0500

    move homer

--- hosts/micro-media/nfs.nix
+++ hosts/micro-media/nfs.nix
@@ -3,7 +3,7 @@
 in {
   fileSystems = {
     "/media" = {
-      device = "${network.nix-nas.tailscale.ip}:/nfs/media";
+      device = "${network.nix-nas.tailscale.ip}:/nfs/media"; # TODO: move to local
       fsType = "nfs";
       options = ["x-systemd.automount" "noauto" "x-systemd.idle-timeout=1200"];
     };
