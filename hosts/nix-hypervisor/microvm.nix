01e47637bf5573c97c0450bc8bafe0092e7ba662
THERAAB <rob.w.lago@gmail.com>
2024-03-08 06:15:51 -0500

    machine-id share

--- hosts/nix-hypervisor/microvm.nix
+++ hosts/nix-hypervisor/microvm.nix
@@ -1,11 +1,11 @@
-{self, ...}: {
+{lib, ...}: {
   systemd.tmpfiles.rules = map (
     vmHost: let
-      machineId = self.lib.addresses.machineId.${vmHost};
+      machineId = lib.addresses.machineId.${vmHost};
     in
       # creates a symlink of each MicroVM's journal under the host's /var/log/journal
       "L+ /var/log/journal/${machineId} - - - - /var/lib/microvms/${vmHost}/journal/${machineId}"
-  ) (builtins.attrNames self.lib.addresses.machineId);
+  ) (builtins.attrNames lib.addresses.machineId);
   microvm = {
     vms = {
       micro-media.config.imports = [
