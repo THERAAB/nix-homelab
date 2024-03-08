1da01df2caa682b39e0ba4dcd0d2d03e084b3cdb
THERAAB <rob.w.lago@gmail.com>
2024-03-08 14:02:50 -0500

    remove tailscale micro-media

--- share/all/users.nix
+++ share/all/users.nix
@@ -1,15 +1,3 @@
 {...}: {
-  users = {
-    mutableUsers = false;
-    users = { #TODO: move
-      # $ nix-shell --run 'mkpasswd -m SHA-512 -s' -p mkpasswd
-      raab = {
-        isNormalUser = true;
-        description = "raab";
-        extraGroups = ["networkmanager" "wheel" "keys"];
-        initialHashedPassword = "$6$89oRObq6lBVqE6Pz$lsm8TXKsGsYZ3HsmbshaTRbecNRMtRQmUUWZS2fubs6y8vF9lLp01dbrhgjGvGPco7qLdEN.hRQ0uisscBanM1";
-        uid = 1000;
-      };
-    };
-  };
+  users.mutableUsers = false;
 }
--- share/physical/nixos/users.nix
+++ share/physical/nixos/users.nix
@@ -8,8 +8,18 @@ in {
       restic.gid = users.restic.gid;
     };
     users = {
-      root.initialHashedPassword = "$6$89oRObq6lBVqE6Pz$lsm8TXKsGsYZ3HsmbshaTRbecNRMtRQmUUWZS2fubs6y8vF9lLp01dbrhgjGvGPco7qLdEN.hRQ0uisscBanM1";
-      root.extraGroups = ["restic"];
+      root = {
+        # $ nix-shell --run 'mkpasswd -m SHA-512 -s' -p mkpasswd
+        initialHashedPassword = "$6$89oRObq6lBVqE6Pz$lsm8TXKsGsYZ3HsmbshaTRbecNRMtRQmUUWZS2fubs6y8vF9lLp01dbrhgjGvGPco7qLdEN.hRQ0uisscBanM1";
+        extraGroups = ["restic"];
+      };
+      raab = {
+        isNormalUser = true;
+        description = "raab";
+        extraGroups = ["networkmanager" "wheel" "keys"];
+        initialHashedPassword = "$6$89oRObq6lBVqE6Pz$lsm8TXKsGsYZ3HsmbshaTRbecNRMtRQmUUWZS2fubs6y8vF9lLp01dbrhgjGvGPco7qLdEN.hRQ0uisscBanM1";
+        uid = 1000;
+      };
       nfsnobody = {
         uid = users.nfsnobody.uid;
         group = "nfsnobody";
