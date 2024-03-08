2722eb8d0cb3c85adb47ac022a90587c872eb5a4
THERAAB <rob.w.lago@gmail.com>
2024-03-08 10:07:38 -0500

    remove tailscale micro-media

--- hosts/nix-nas/nfs.nix
+++ hosts/nix-nas/nfs.nix
@@ -22,7 +22,7 @@ in {
       options = ["subvol=backups" "compress=zstd" "noatime"];
     };
   };
-  networking.firewall.allowedTCPPorts = [2049 111 20048];
+  networking.firewall.allowedTCPPorts = [2049 111 20048 864];
   services.nfs.server = {
     enable = true;
     exports = ''
