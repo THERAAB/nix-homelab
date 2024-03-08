f913b4a4f942c4e486589aa5b99f61f8ef199664
THERAAB <rob.w.lago@gmail.com>
2024-03-08 07:56:49 -0500

    machine-id share

--- hosts/nix-hypervisor/microvm.nix
+++ hosts/nix-hypervisor/microvm.nix
@@ -1,10 +1,9 @@
-{config, ...}: let
+{...}: let
   network = import ../../share/network.properties.nix;
-  inherit (config.microvm.vms.micro-media.config.systemd) machine-id;
 in {
   systemd.tmpfiles.rules = [
     # Share journald logs on nix-hypervisor
-    "L+ /var/log/journal/${machine-id}      -   -   -   -   /var/lib/microvms/micro-media/storage/journal/${network.micro-media.machine-id}         "
+    "L+ /var/log/journal/${network.micro-media.machine-id}      -   -   -   -   /var/lib/microvms/micro-media/storage/journal/${network.micro-media.machine-id}         "
     "L+ /var/log/journal/${network.micro-server.machine-id}     -   -   -   -   /var/lib/microvms/micro-server/storage/journal/${network.micro-server.machine-id}       "
     "L+ /var/log/journal/${network.micro-tailscale.machine-id}  -   -   -   -   /var/lib/microvms/micro-tailscale/storage/journal/${network.micro-tailscale.machine-id} "
     "L+ /var/log/journal/${network.micro-utils.machine-id}      -   -   -   -   /var/lib/microvms/micro-utils/storage/journal/${network.micro-utils.machine-id}         "
