4ad2be79243b673a76456b2e09f9de9578c850b5
THERAAB <rob.w.lago@gmail.com>
2024-03-09 07:08:21 -0500

    olivetin

--- hosts/nix-hypervisor/olivetin/default.nix
+++ hosts/nix-hypervisor/olivetin/default.nix
@@ -4,7 +4,7 @@
   gid = 62893;
   app-name = "olivetin";
   www-dir = "/var/www/${app-name}";
-  scripts-dir = "/var/lib/OliveTin/scripts";
+  scripts-dir = "/var/lib/olivetin/scripts";
   shellScript = pkgs.callPackage ./script.nix {};
   system-icons-dir = "/nix/persist/nix-homelab/share/assets/icons";
 in {
