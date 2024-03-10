4fb1ad4143ef3d8ecc7847a8b27384f4e4f66fb5
THERAAB <rob.w.lago@gmail.com>
2024-03-10 18:26:34 -0400

    remove todo

--- share/microvm/hardware.nix
+++ share/microvm/hardware.nix
@@ -13,13 +13,13 @@ in {
   microvm = {
     hypervisor = "cloud-hypervisor";
     shares = [
-      #{
-      #  #TODO ?
-      #  source = "/nix/store";
-      #  mountPoint = "/nix/.ro-store";
-      #  tag = "ro-store";
-      #  proto = "virtiofs";
-      #}
+      {
+        #TODO ?
+        source = "/nix/store";
+        mountPoint = "/nix/.ro-store";
+        tag = "ro-store";
+        proto = "virtiofs";
+      }
       {
         source = "/var/lib/microvms/${config.networking.hostName}/storage/journal";
         mountPoint = "/var/log/journal";
