b888df0b9a56eca298b97dcf9f4bd614bc18d357
THERAAB <rob.w.lago@gmail.com>
2024-03-10 11:28:52 -0400

    imperative microvm

--- hosts/micro-automate/home-assistant/govee-immersion.nix
+++ hosts/micro-automate/home-assistant/govee-immersion.nix
@@ -19,7 +19,7 @@ in {
               hash = "sha256-6nZzc3zY9UXGFK7r1SeOMzEzIwakW5anbu7lJwWqwI4=";
             };
             propagatedBuildInputs = [
-              pkgs.bios
+              # pkgs.bios TODO: fix
               pexpect
               events
               pygatt
