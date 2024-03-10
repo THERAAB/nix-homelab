079a36e085172c79e6867c6b114c13e93c929421
THERAAB <rob.w.lago@gmail.com>
2024-03-10 12:19:30 -0400

    imperative microvm

--- share/microvm/hardware.nix
+++ share/microvm/hardware.nix
@@ -12,12 +12,12 @@ in {
   microvm = {
     hypervisor = "cloud-hypervisor";
     shares = [
-      { #TODO: remove by moving to imperative updates
-        source = "/nix/store";
-        mountPoint = "/nix/.ro-store";
-        tag = "ro-store";
-        proto = "virtiofs";
-      }
+      #{ #TODO: remove by moving to imperative updates
+      #  source = "/nix/store";
+      #  mountPoint = "/nix/.ro-store";
+      #  tag = "ro-store";
+      #  proto = "virtiofs";
+      #}
       {
         source = "/var/lib/microvms/${config.networking.hostName}/storage/journal";
         mountPoint = "/var/log/journal";
