cd1eb4898691dfb5c2539f04ed2a43fc605ad7ce
THERAAB <rob.w.lago@gmail.com>
2024-03-12 06:37:09 -0400

    update unstable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "nix-homelab flake";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
-    nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager/release-23.11";
       inputs.nixpkgs.follows = "nixpkgs";
