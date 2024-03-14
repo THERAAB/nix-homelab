dca653eb3c9d1e6c04884794547802431d1ef82d
THERAAB <rob.w.lago@gmail.com>
2024-03-14 10:29:09 -0400

    shared modules

--- homes/x86_64-linux/raab@nix-hypervisor/default.nix
+++ homes/x86_64-linux/raab@nix-hypervisor/default.nix
@@ -1,9 +1,3 @@
 {...}: {
-  nix-homelab.physical = {
-    fish.enable = true;
-    git.enable = true;
-    nvim.enable = true;
-    home.enable = true;
-    persist.enable = true;
-  };
+  nix-homelab.physical.enable = true;
 }
---
+++ modules/home/physical/default.nix
@@ -0,0 +1,22 @@
+{
+  lib,
+  config,
+  ...
+}:
+with lib;
+with lib.nix-homelab; let
+  cfg = config.nix-homelab.physical;
+in {
+  options.nix-homelab.physical = with types; {
+    enable = mkEnableOption (lib.mdDoc "System");
+  };
+  config = mkIf cfg.enable {
+    nix-homelab.physical = {
+      fish.enable = true;
+      git.enable = true;
+      nvim.enable = true;
+      home.enable = true;
+      persist.enable = true;
+    };
+  };
+}
