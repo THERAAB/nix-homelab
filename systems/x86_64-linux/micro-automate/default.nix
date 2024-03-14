9801a0178437ed32761fe9d191427cdca9aad2e1
THERAAB <rob.w.lago@gmail.com>
2024-03-14 07:34:54 -0400

    shared modules

--- systems/x86_64-linux/micro-automate/default.nix
+++ systems/x86_64-linux/micro-automate/default.nix
@@ -1,6 +1,26 @@
-{...}: {
+{...}: let
+  custom-blueprints-dir = "/var/lib/hass/blueprints/automation/custom";
+  system-blueprints-dir = "/nix/persist/nix-homelab/systems/x86_64-linux/micro-automate/home-assistant/blueprints";
+in {
   imports = [
     ./hardware.nix
     ./home-assistant
   ];
+  nix-homelab.services.home-assistant.enable = true;
+  microvm = {
+    devices = [
+      {
+        bus = "pci";
+        path = "0000:00:14.0"; # Passthrough whole USB bus because single usb device passthrough doesn't work
+      }
+    ];
+    shares = [
+      {
+        proto = "virtiofs";
+        source = system-blueprints-dir;
+        mountPoint = custom-blueprints-dir;
+        tag = "custom-blueprints";
+      }
+    ];
+  };
 }
