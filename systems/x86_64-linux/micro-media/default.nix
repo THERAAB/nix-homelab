17bec270e74d9b7d1ebd63638954720adf1a5ad0
THERAAB <rob.w.lago@gmail.com>
2024-03-14 08:11:33 -0400

    shared modules

--- systems/x86_64-linux/micro-media/default.nix
+++ systems/x86_64-linux/micro-media/default.nix
@@ -1,9 +1,19 @@
 {...}: {
   imports = [
-    ./jellyfin.nix
-    ./jellyseerr.nix
-    ./audiobookshelf.nix
     ./hardware.nix
   ];
-  nix-homelab.media.enable = true;
+  nix-homelab = {
+    media.enable = true;
+    services = {
+      audiobookshelf.enable = true;
+      jellyfin.enable = true;
+      jellyseerr.enable = true;
+    };
+  };
+  microvm.devices = [
+    {
+      bus = "pci";
+      path = "0000:00:02.0";
+    }
+  ];
 }
