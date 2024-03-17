207868362752d80a9e0190056e54c3599f891e81
THERAAB <rob.w.lago@gmail.com>
2024-03-17 19:04:24 -0400

    loki

--- systems/x86_64-linux/nix-hypervisor/grafana.nix
+++ systems/x86_64-linux/nix-hypervisor/grafana.nix
@@ -35,6 +35,58 @@
   services.loki = {
     enable = true;
     configuration = {
+      auth_enabled = false;
+      server.http_listen_port = 3100;
+      ingester = {
+        lifecycler = {
+          address = "0.0.0.0";
+          ring = {
+            kvstore.store = "inmemory";
+            replication_factor = 1;
+          };
+          final_sleep = "0s";
+        };
+        chunk_idle_period = "1h";
+        max_chunk_age = "1h";
+        chunk_target_size = 1048576;
+        chunk_retain_period = "30s";
+        max_transfer_retries = 0;
+      };
+      schema_config = {
+        configs = {
+          from = "2020-10-24";
+          store = "boltdb-shipper";
+          object_store = "filesystem";
+          schema = "v11";
+          index = {
+            prefix = "index_";
+            period = "24h";
+          };
+        };
+      };
+      storage_config = {
+        boltdb_shipper = {
+          active_index_directory = "/var/lib/loki/boltdb-shipper-active";
+          cache_location = "/var/lib/loki/boltdb-shipper-cache";
+          cache_ttl = "24h";
+          shared_store = "filesystem";
+        };
+        filesystem.directory = "/var/lib/loki/chunks";
+      };
+      limits_config = {
+        reject_old_samples = true;
+        reject_old_samples_max_age = "168h";
+      };
+      chunk_store_config.max_look_back_period = "0s";
+      table_manager = {
+        retention_deletes_enabled = false;
+        retention_period = "0s";
+      };
+    };
+  };
+  services.promtail = {
+    enable = true;
+    configuration = {
       server = {
         http_listen_port = properties.ports.loki;
         grpc_listen_port = 0;
