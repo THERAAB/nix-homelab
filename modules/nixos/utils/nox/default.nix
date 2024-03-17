aac6dc321054664c3ff501dce8c3acfe42a820a1
THERAAB <rob.w.lago@gmail.com>
2024-03-16 20:23:38 -0400

    automated commit

--- modules/nixos/utils/nox/default.nix
+++ modules/nixos/utils/nox/default.nix
@@ -13,7 +13,6 @@ in {
   };
   config = mkIf cfg.enable {
     environment.systemPackages = [
-      #TODO improve
       (
         pkgs.writeScriptBin "nox" ''
 
@@ -34,7 +33,7 @@ in {
           commit() {
               $git -C $dir pull
               $git -C $dir add .
-              $git -C $dir commit -m "automated commit"
+              $git -C $dir commit -m "$1"
               $git -C $dir push
           }
           status() {
@@ -50,26 +49,39 @@ in {
           pull() {
               $git -C $dir pull
           }
+          apply() {
+              $git -C $dir pull
+              sudo nix run $dir#apps.nixinate.$1
+          }
+          micro_local() {
+            if [ $# -lt 1 ]; then
+              NAMES="$(ls -1 /var/lib/microvms)"
+            else
+              shift
+              NAMES="$@"
+            fi
+
+            for NAME in $NAMES; do
+              echo $NAME
+              sudo microvm -Ru $NAME
+            done
+          }
+
           case "$1" in
               inputs) inputs;;
               rebuild) rebuild;;
-              commit) commit;;
+              commit) commit $2;;
               update) update;;
               gc) gc;;
               status) status;;
               pull) pull;;
+              apply) apply $2;;
+              micro-local)
+                shift
+                micro_local $@;;
               micro)
-                if [ $# -lt 2 ]; then
-                  NAMES="$(ls -1 /var/lib/microvms)"
-                else
-                  shift
-                  NAMES="$@"
-                fi
-
-                for NAME in $NAMES; do
-                  echo $NAME
-                  sudo microvm -Ru $NAME
-                done
+                shift
+                sudo ${pkgs.openssh}/bin/ssh -t raab@nix-hypervisor "sudo flock -w 60 /dev/shm/nox-micro nox micro $@";;
           esac
         ''
       )
