af4ef48f694f67765e659f8a7c6a7c8cd19bd342
THERAAB <rob.w.lago@gmail.com>
2024-03-17 18:51:13 -0400

    loki

--- systems/x86_64-linux/nix-hypervisor/grafana.nix
+++ systems/x86_64-linux/nix-hypervisor/grafana.nix
@@ -37,6 +37,23 @@
     configuration = {
       server = {
         http_listen_port = properties.ports.loki;
+        grpc_listen_port = 0;
+      };
+      positions.filename = "/tmp/positions.yaml";
+      clients.url = "http://127.0.0.1:3100/loki/api/v1/push";
+      scrape_configs = {
+        job_name = "journal";
+        journal = {
+          max_age = "12h";
+          labels = {
+            job = "systemd-journal";
+            host = "node-exporter";
+          };
+        };
+        relabel_configs = {
+          source_labels = ["__journal__systemd_unit"];
+          target_label = "unit";
+        };
       };
     };
   };
