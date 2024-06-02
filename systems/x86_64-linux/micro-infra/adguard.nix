7c18d855314908335a1d9ad4849f71d82b3c7522
THERAAB <rob.w.lago@gmail.com>
2024-06-02 07:08:27 -0400

    automated commit

--- systems/x86_64-linux/micro-infra/adguard.nix
+++ systems/x86_64-linux/micro-infra/adguard.nix
@@ -3,23 +3,21 @@
   services.adguardhome = {
     host = properties.network.micro-infra.local.ip;
     settings = {
-      dns = {
-        bind_hosts = [properties.network.micro-infra.local.ip];
-        rewrites = [
-          {
-            domain = "pfsense.${properties.network.domain}";
-            answer = "${properties.network.pfSense.local.ip}";
-          }
-          {
-            domain = "${properties.network.domain}";
-            answer = "${properties.network.micro-tailscale.local.ip}";
-          }
-          {
-            domain = "*.${properties.network.domain}";
-            answer = "${properties.network.micro-tailscale.local.ip}";
-          }
-        ];
-      };
+      dns.bind_hosts = [properties.network.micro-infra.local.ip];
+      filtering.rewrites = [
+        {
+          domain = "pfsense.${properties.network.domain}";
+          answer = "${properties.network.pfSense.local.ip}";
+        }
+        {
+          domain = "${properties.network.domain}";
+          answer = "${properties.network.micro-tailscale.local.ip}";
+        }
+        {
+          domain = "*.${properties.network.domain}";
+          answer = "${properties.network.micro-tailscale.local.ip}";
+        }
+      ];
     };
   };
 }
--- systems/x86_64-linux/micro-tailscale/adguard-tailscale.nix
+++ systems/x86_64-linux/micro-tailscale/adguard-tailscale.nix
@@ -3,23 +3,21 @@
   services.adguardhome = {
     host = properties.network.micro-tailscale.local.ip;
     settings = {
-      dns = {
-        bind_hosts = [properties.network.micro-tailscale.tailscale.ip properties.network.micro-tailscale.local.ip];
-        rewrites = [
-          {
-            domain = "pfsense.${properties.network.domain}";
-            answer = "${properties.network.pfSense.local.ip}";
-          }
-          {
-            domain = "${properties.network.domain}";
-            answer = "${properties.network.micro-tailscale.tailscale.ip}";
-          }
-          {
-            domain = "*.${properties.network.domain}";
-            answer = "${properties.network.micro-tailscale.tailscale.ip}";
-          }
-        ];
-      };
+      dns.bind_hosts = [properties.network.micro-tailscale.tailscale.ip properties.network.micro-tailscale.local.ip];
+      filtering.rewrites = [
+        {
+          domain = "pfsense.${properties.network.domain}";
+          answer = "${properties.network.pfSense.local.ip}";
+        }
+        {
+          domain = "${properties.network.domain}";
+          answer = "${properties.network.micro-tailscale.tailscale.ip}";
+        }
+        {
+          domain = "*.${properties.network.domain}";
+          answer = "${properties.network.micro-tailscale.tailscale.ip}";
+        }
+      ];
     };
   };
 }
