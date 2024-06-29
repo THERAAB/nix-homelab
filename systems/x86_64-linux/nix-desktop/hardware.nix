9fa66182a43db7ddd4af1dfa524431fd6778b086
THERAAB <rob.w.lago@gmail.com>
2024-06-29 07:58:02 -0400

    automated commit

--- systems/x86_64-linux/nix-desktop/hardware.nix
+++ systems/x86_64-linux/nix-desktop/hardware.nix
@@ -39,11 +39,11 @@
           TIMELINE_CREATE = true;
           TIMELINE_CLEANUP = true;
           TIMELINE_MIN_AGE = 7200; # 2 hours
-          TIMELINE_LIMIT_HOURLY = 10;
-          TIMELINE_LIMIT_DAILY = 10;
-          TIMELINE_LIMIT_WEEKLY = 4;
-          TIMELINE_LIMIT_MONTHLY = 0;
-          TIMELINE_LIMIT_YEARLY = 0;
+          TIMELINE_LIMIT_HOURLY = "10";
+          TIMELINE_LIMIT_DAILY = "10";
+          TIMELINE_LIMIT_WEEKLY = "4";
+          TIMELINE_LIMIT_MONTHLY = "0";
+          TIMELINE_LIMIT_YEARLY = "0";
         };
       };
     };
