fb613a58c1e4c1664d08e3a49ccba7e396aa52f2
THERAAB <rob.w.lago@gmail.com>
2024-12-05 09:38:24 -0500

    automated commit

--- systems/x86_64-linux/micro-media/hardware.nix
+++ systems/x86_64-linux/micro-media/hardware.nix
@@ -2,22 +2,22 @@
   networking.hostName = "micro-media";
   microvm = {
     mem = 6144;
-    interfaces = [
-      {
-        type = "macvtap";
-        macvtap = {
-          mode = "bridge";
-          link = "enp3s0";
-        };
-        id = config.networking.hostName;
-        mac = "02:00:00:00:00:01";
-      }
-    ];
-    devices = [
-      {
-        bus = "pci";
-        path = "0000:00:02.0";
-      }
-    ];
+    #interfaces = [
+    #  {
+    #    type = "macvtap";
+    #    macvtap = {
+    #      mode = "bridge";
+    #      link = "enp3s0";
+    #    };
+    #    id = config.networking.hostName;
+    #    mac = "02:00:00:00:00:01";
+    #  }
+    #];
+    #devices = [
+    #  {
+    #    bus = "pci";
+    #    path = "0000:00:02.0";
+    #  }
+    #];
   };
 }
