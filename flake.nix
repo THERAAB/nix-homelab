6d0d02c2e5f4133242b917cd512d01c64f821558
THERAAB <rob.w.lago@gmail.com>
2024-12-16 13:30:54 -0500

    Revert "automated commit"
    This reverts commit 06c854fd4c557e7eca025a82a87cede0025b188c.

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     microvm = {
-      url = "github:astro/microvm.nix/0ab757d2d3e3214b0034b00f9cc3dcdba0b8c563";
+      url = "github:astro/microvm.nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     snowfall-lib = {
