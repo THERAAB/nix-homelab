6732eed2fa05f3d3d0b54f88b990f66b798aea44
THERAAB <rob.w.lago@gmail.com>
2024-12-30 09:29:07 -0500

    automated commit

--- packages/plugp100/default.nix
+++ packages/plugp100/default.nix
@@ -1,15 +1,15 @@
 {
-  python3Packages,
+  python312Packages,
   fetchPypi,
 }:
-python3Packages.buildPythonPackage rec {
+python312Packages.buildPythonPackage rec {
   pname = "plugp100";
   version = "3.12.0";
   src = fetchPypi {
     inherit version pname;
     hash = "sha256-jfitt8qCq8nRbCgv81qz3Wtwp0R9fXQ7TjP88Sh2oIY=";
   };
-  propagatedBuildInputs = with python3Packages; [
+  propagatedBuildInputs = with python312Packages; [
     certifi
     cryptography
     jsons
