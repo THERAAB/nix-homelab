eff0e67a76d09b2b3e388cf4cb412fc68bf687a6
THERAAB <rob.w.lago@gmail.com>
2025-01-19 08:56:09 -0500

    automated commit

--- assets/properties/network.nix
+++ assets/properties/network.nix
@@ -45,7 +45,6 @@
   unifi-usw-lite-8.local.ip = "192.168.1.3";
   govee-water-alarm.local.ip = "192.168.127.3";
   ring-doorbell.local.ip = "192.168.127.4";
-  kasa-living-room-plug.local.ip = "192.168.127.6";
   android-tv.local.ip = "192.168.127.5";
   b-hyve.local.ip = "192.168.127.7";
   ecobee.local.ip = "192.168.127.8";
