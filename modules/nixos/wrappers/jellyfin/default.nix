624c94a8573bdf877dbd7570fa8ef38d9efe9ea7
THERAAB <rob.w.lago@gmail.com>
2025-11-23 06:31:31 -0500

    automated commit

--- modules/nixos/wrappers/jellyfin/default.nix
+++ modules/nixos/wrappers/jellyfin/default.nix
@@ -35,8 +35,9 @@ in {
       enable = true;
       extraPackages = with pkgs; [
         intel-media-driver # LIBVA_DRIVER_NAME=iHD
-        vaapiIntel # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
         libva-vdpau-driver
+        intel-vaapi-driver # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+        vaapiVdpau
         libvdpau-va-gl
       ];
     };
