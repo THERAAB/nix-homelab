b54baa7d68549671f018c85f1e76fe75bc168369
THERAAB <rob.w.lago@gmail.com>
2026-03-03 07:05:26 -0500

    automated commit

--- modules/home/physical/persist/default.nix
+++ modules/home/physical/persist/default.nix
@@ -14,6 +14,7 @@ in {
     home.persistence."/nix/persist" = {
       directories = [
         ".config/sops/age"
+        ".config/nix"
         ".config/fish"
         ".cache.tealdeer"
         ".cache/nvim"
