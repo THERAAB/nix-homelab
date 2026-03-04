58de1407c6cf22c334e2eb3b64eb7e03201af15d
THERAAB <rob.w.lago@gmail.com>
2026-03-04 14:08:47 -0500

    committed using nox

--- systems/x86_64-linux/nix-hypervisor/homer.nix
+++ systems/x86_64-linux/nix-hypervisor/homer.nix
@@ -7,33 +7,7 @@ in {
   logo = "assets/icons/nixos.png";
   header = true;
   footer = false;
-  theme = "default";
-  colors = {
-    dark = {
-      highlight-primary = "transparent";
-      highlight-secondary = "#414559";
-      highlight-hover = "#414559";
-      background = "#303446";
-      card-background = "#414559";
-      text = "#c6d0f5";
-      text-header = "#c6d0f5";
-      text-title = "#c6d0f5";
-      text-subtitle = "#ca9ee6";
-      card-shadow = "rgba(0, 0, 0, 0.1)";
-    };
-    light = {
-      highlight-primary = "transparent";
-      highlight-secondary = "#e6e9ef";
-      highlight-hover = "#e6e9ef";
-      background = "#eff1f5";
-      card-background = "#e6e9ef";
-      text = "#4c4f69";
-      text-header = "#4c4f69";
-      text-title = "#4c4f69";
-      text-subtitle = "#ea76cb";
-      card-shadow = "rgba(0, 0, 0, 0.1)";
-    };
-  };
+  theme = "walkxcode";
   links = [
     {
       name = "GitHub";
