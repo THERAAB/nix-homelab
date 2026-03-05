ec135b1f78dae9504ea33a7088ff5bee8a818cd9
THERAAB <rob.w.lago@gmail.com>
2026-03-05 09:13:58 -0500

    committed using nox

--- modules/nixos/wrappers/immich/default.nix
+++ modules/nixos/wrappers/immich/default.nix
@@ -8,18 +8,24 @@ with lib;
 with lib.nix-homelab; let
   cfg = config.nix-homelab.wrappers.immich;
   port = properties.ports.immich;
+  media-dir = "/sync/immich";
 in {
   options.nix-homelab.wrappers.immich = with types; {
     enable = mkEnableOption (lib.mdDoc "Immich");
   };
   config = mkIf cfg.enable {
+    systemd.tmpfiles.rules = [
+      "d    ${media-dir}     -    -         -        -   - "
+      "Z    ${media-dir}     -    immich   immich    -   - "
+    ];
     services.immich = {
       enable = true;
       port = port;
       host = "0.0.0.0";
       openFirewall = true;
       accelerationDevices = ["/dev/dri/renderD128"];
+      mediaLocation = media-dir;
     };
-    users.users.immich.extraGroups = ["video" "render"];
+    users.users.immich.extraGroups = ["video" "render" "syncthing"];
   };
 }
