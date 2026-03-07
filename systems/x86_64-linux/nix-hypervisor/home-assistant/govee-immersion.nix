bb08e0207cdbe2ecfce27be63859a856bc9c7c84
THERAAB <rob.w.lago@gmail.com>
2026-03-07 10:58:47 -0500

    committed using nox

--- systems/x86_64-linux/nix-hypervisor/home-assistant/govee-immersion.nix
+++ systems/x86_64-linux/nix-hypervisor/home-assistant/govee-immersion.nix
@@ -2,38 +2,36 @@
   devices = import ./devices.properties.nix;
 in {
   nix-homelab.wrappers.govee2mqtt.enable = true;
-  services.home-assistant = {
-    config.automation = [
-      {
-        alias = "Turn on Govee with TV";
-        trigger = {
-          platform = "state";
-          entity_id = devices.entity-id.living-room.android-tv;
-          from = "off";
-          to = "idle";
+  services.home-assistant.config.automation = [
+    {
+      alias = "Turn on Govee with TV";
+      trigger = {
+        platform = "state";
+        entity_id = devices.entity-id.living-room.android-tv;
+        from = "off";
+        to = "idle";
+      };
+      action = {
+        service = "light.turn_on";
+        target = {
+          entity_id = devices.entity-id.living-room.govee-immersion;
         };
-        action = {
-          service = "light.turn_on";
-          target = {
-            entity_id = devices.entity-id.living-room.govee-immersion;
-          };
+      };
+    }
+    {
+      alias = "Turn off Govee with TV";
+      trigger = {
+        platform = "state";
+        entity_id = devices.entity-id.living-room.android-tv;
+        from = "idle";
+        to = "off";
+      };
+      action = {
+        service = "light.turn_off";
+        target = {
+          entity_id = devices.entity-id.living-room.govee-immersion;
         };
-      }
-      {
-        alias = "Turn off Govee with TV";
-        trigger = {
-          platform = "state";
-          entity_id = devices.entity-id.living-room.android-tv;
-          from = "idle";
-          to = "off";
-        };
-        action = {
-          service = "light.turn_off";
-          target = {
-            entity_id = devices.entity-id.living-room.govee-immersion;
-          };
-        };
-      }
-    ];
-  };
+      };
+    }
+  ];
 }
