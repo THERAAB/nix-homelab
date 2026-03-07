8bfbf3eff1c6ad2693b6753ad3c40847c5e5c267
THERAAB <rob.w.lago@gmail.com>
2026-03-07 16:28:39 -0500

    committed using nox

--- systems/x86_64-linux/nix-hypervisor/olivetin/default.nix
+++ systems/x86_64-linux/nix-hypervisor/olivetin/default.nix
@@ -1,23 +1,16 @@
 {
-  self,
   properties,
   pkgs,
   ...
 }: let
   port = properties.ports.olivetin;
-  app-name = "olivetin";
-  icons-dir = "/var/lib/${app-name}/customIcons";
   scripts-dir = "/var/lib/olivetin/scripts";
   shellScript = pkgs.callPackage ./script.nix {};
-  system-icons-dir = self + "/assets/icons";
 in {
   systemd.tmpfiles.rules = [
     "r  ${scripts-dir}/commands.sh    -           -               -               -   -                                     "
     "L  ${scripts-dir}/commands.sh    -           -               -               -   ${shellScript}                        "
-    "R  ${icons-dir}                  -           -               -               -   -                                     "
-    "C  ${icons-dir}                 -           -               -               -   ${system-icons-dir}                   "
     "Z  ${scripts-dir}                500         root            root            -   -                                     "
-    "Z  ${icons-dir}                  -           olivetin        olivetin        -   -                                     "
   ];
   services.olivetin = {
     enable = true;
@@ -26,8 +19,8 @@ in {
       actions = [
         {
           title = "Reboot Nix-Hypervisor";
-          icon = ''<img src = "customIcons/reboot.png" width = "48px"/>'';
-          shell = "/run/wrappers/bin/sudo /var/lib/olivetin/scripts/commands.sh -r";
+          icon = ''<iconify-icon icon="devicon:nixos"></iconify-icon>'';
+          shell = "/run/wrappers/bin/sudo ${scripts-dir}/commands.sh -r";
           timeout = 20;
         }
       ];
