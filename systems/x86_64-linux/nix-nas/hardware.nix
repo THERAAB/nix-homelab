c8f58d1eaf943883956ea91015eb34dd0489f74f
THERAAB <rob.w.lago@gmail.com>
2026-03-08 19:53:22 -0400

    committed using nox

--- systems/x86_64-linux/nix-nas/hardware.nix
+++ systems/x86_64-linux/nix-nas/hardware.nix
@@ -33,7 +33,6 @@ in {
     hostName = "nix-nas";
     networkmanager.enable = false; # networkmanager hangs on poweroff due to some ipv6 issues
   };
-  # TODO: this no longer works, why?
   # disable interrupt which uses high CPU for ACPI (IRQ 9)
   # Found it from highest number in below command:
   # sudo grep . -r /sys/firmware/acpi/interrupts | grep -v "  0"
