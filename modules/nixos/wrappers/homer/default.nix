3ee556641abbd0598729dd6209b6c1ff57a222e5
THERAAB <rob.w.lago@gmail.com>
2026-03-08 13:46:01 -0400

    committed using nox

--- modules/nixos/wrappers/homer/default.nix
+++ modules/nixos/wrappers/homer/default.nix
@@ -72,7 +72,7 @@ in {
             items = [
               {
                 name = "Jellyfin";
-                logo = "${self + assets/icons/jellyfin.png}";
+                logo = "${./icons/jellyfin.png}";
                 subtitle = "Watch Movies & TV";
                 tag = "media";
                 keywords = "movies tv shows media";
