c36c38f4d6f2a2a642d8e063a166684c6cd13f4e
THERAAB <rob.w.lago@gmail.com>
2026-03-22 17:16:32 -0400

    committed using nox

--- modules/home/workstation/vicinae/default.nix
+++ modules/home/workstation/vicinae/default.nix
@@ -1,6 +1,7 @@
 {
   lib,
   config,
+  pkgs,
   ...
 }:
 with lib;
@@ -16,12 +17,37 @@ in {
       systemd = {
         enable = true;
       };
+      extensions = [
+        (config.lib.vicinae.mkExtension {
+          name = "nix";
+          src =
+            pkgs.fetchFromGitHub {
+              owner = "vicinaehq";
+              repo = "extensions";
+              rev = "50233dff9dfc70fc6b39c2387687e5661b09f005";
+              sha256 = "sha256-GVIbXYiA506LV0cEsG1AA4vTwDJq9R6v6lFFs8z7knY=";
+            }
+            + "/extensions/nix";
+        })
+        (config.lib.vicinae.mkExtension {
+          name = "firefox";
+          src =
+            pkgs.fetchFromGitHub {
+              owner = "vicinaehq";
+              repo = "extensions";
+              rev = "50233dff9dfc70fc6b39c2387687e5661b09f005";
+              sha256 = "sha256-GVIbXYiA506LV0cEsG1AA4vTwDJq9R6v6lFFs8z7knY=";
+            }
+            + "/extensions/firefox";
+        })
+      ];
       settings = {
         font.family = "system";
         launcher_window.opacity = lib.mkForce 0.95;
         pop_to_root_on_close = true;
         theme.dark.icon_theme = "Papirus-Dark";
         favorites = [];
+        activate_on_single_click = true;
       };
     };
   };
