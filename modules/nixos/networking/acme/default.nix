9ed684caeec8ff9b01731ee04cbd4cf03ac0b0f5
THERAAB <rob.w.lago@gmail.com>
2026-07-30 13:09:49 -0400

    committed using nox

--- modules/nixos/networking/acme/default.nix
+++ modules/nixos/networking/acme/default.nix
@@ -24,7 +24,7 @@ in
       defaults = {
         email = "example@aol.com";
         credentialFiles = {
-          CF_ZONE_API_TOKEN_FILE = "/run/secrets/cloudflare_dns_secret";
+          DNS_SECRET_FILE = "/run/secrets/cloudflare_dns_secret";
         };
         dnsProvider = "cloudflare";
         dnsResolver = "1.1.1.1:${toString properties.ports.dns}";
